7a64796a3ec0e5dfab5e21a59ab17521baa3b199
Phil Dudero <dudero@physics.umn.edu>
2010-12-01 00:50:00 +0000

    add getMassPoints()

--- NeuralNets/interface/HeavyNuNetworks.h
+++ NeuralNets/interface/HeavyNuNetworks.h
@@ -2,10 +2,12 @@
 #define HeavyNuNetworks_h_included 1
 
 #include <vector>
+#include <utility>
 
 class HeavyNuNetworks {
  public:
   static float evaluate(int mw, int mnu, const std::vector<float>& invalstd);
+  static std::vector<std::pair<int,int> > getMassPoints();
 };
 
 #endif // HeavyNuNetworks_h_included
