245351ac53f695b04092ae4b6e29fa4067d99324
Phil Dudero <dudero@physics.umn.edu>
2010-12-08 14:21:22 +0000

    first commit

---
+++ AnalysisModules/python/pythia6_tauola_ttbar_mumu_tuneZ2_reco385_28.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+readFiles = cms.untracked.vstring()
+source = cms.Source("PoolSource",fileNames=readFiles)
+readFiles.extend( [
+'file:/local/cms/user/jmmans/PYTHIA6_Tauola_TTbar_mumu_TuneZ2_7TeV_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RECO_385/PYTHIA6_Tauola_TTbar_mumu_TuneZ2_7TeV_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RECO_028.root'
+])
