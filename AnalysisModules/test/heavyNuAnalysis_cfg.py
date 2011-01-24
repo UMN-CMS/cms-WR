cdafa39f5c41438a7709eb2c6eb2ab1249730fdc
Phil Dudero <dudero@physics.umn.edu>
2011-01-24 23:25:29 +0000

    add trigger sim. random seed for MC, factor out modules to separate cfi files, remove JES paths for separate config file

--- AnalysisModules/test/heavyNuAnalysis_cfg.py
+++ AnalysisModules/test/heavyNuAnalysis_cfg.py
@@ -1,14 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
+import os
+
 #import sys
 #isMC=sys.modules['__main__'].isMC
 #isMCsignal=sys.modules['__main__'].isMCsignal
 #process = sys.modules['__main__'].process
 
-isMC=False
+isMC=True
 isMCsignal=False
 Training=False
-isRun2010LoLumi=True
+isRun2010LoLumi=False
 
 isData=not isMC
 
@@ -22,7 +24,7 @@ isRun2010HiLumi=not isRun2010LoLumi
 process = cms.Process("PAT");
 
 process.load('FWCore/MessageService/MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound'),
@@ -31,8 +33,9 @@ process.options = cms.untracked.PSet(
 # source
 #process.source = cms.Source("PoolSource",
 #    fileNames=cms.untracked.vstring('file:/local/cms/user/dahmes/wr2010/muSCjetSkim/run2010B/nov7/mySkim/mySkim-nov7_056005.root')
-#     fileNames=cms.untracked.vstring('/store/mc/Fall10/Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola/GEN-SIM-RECO/START38_V12-v2/0018/027050CA-D50B-E011-91DD-00261894391C.root')
-##    fileNames=cms.untracked.vstring( "file:/local/cms/user/dudero/HeavyNuRecoFromHLT/WR1000_nuRmu100/HeavyNuGenHLT_WR1000_nuRmu100_1-reco-pool.root" )
+#This file is AT FNAL:
+#    fileNames=cms.untracked.vstring('/store/mc/Fall10/Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola/GEN-SIM-RECO/START38_V12-v2/0018/027050CA-D50B-E011-91DD-00261894391C.root')
+#    fileNames=cms.untracked.vstring( "file:/hdfs/cms/user/heavynu/HeavyNuRecoFromHLT/38X/WR1000_nuRmu100/HeavyNuGenHLT_WR1000_nuRmu100_1-reco-pool.root" )
 #)
 process.load('HeavyNu.AnalysisModules.in_cff')
 
@@ -109,23 +112,7 @@ else:
 ########################################
 # imported directly from PhysicsTools/PatExamples/test/analyzePatTrigger_onTheFly_cfg.py
 #
-# PAT trigger
-process.muonTriggerMatchHLTMuons = cms.EDProducer (
-    "PATTriggerMatcherDRDPtLessByR",
-    src            = cms.InputTag( 'selectedPatMuons' ),
-    matched        = cms.InputTag( 'patTrigger' ),
-    andOr          = cms.bool( False ),
-    filterIdsEnum  = cms.vstring( 'TriggerMuon' ),
-    filterIds      = cms.vint32( 0 ),
-    filterLabels   = cms.vstring( '*' ),
-    pathNames      = cms.vstring(),
-    collectionTags = cms.vstring( '*' ),
-    maxDPtRel      = cms.double( 1.0 ),
-    maxDeltaR      = cms.double( 0.2 ),
-    maxDeltaEta    = cms.double( 0.2 ), # no effect here
-    resolveAmbiguities    = cms.bool( True ),
-    resolveByMatchQuality = cms.bool( True )
-)
+process.load("HeavyNu.AnalysisModules.hnutrigmatch_cfi")
 
 ### ============
 ### Python tools
@@ -156,40 +143,15 @@ process.TFileService = cms.Service("TFileService",
        fileName = cms.string("anal.root"),
 )
 
-process.hNu = cms.EDFilter(
-    "HeavyNu",
-    trigMatchPset = cms.PSet(
-    trigEventTag = cms.InputTag( "" ),
-    muonMatch    = cms.string( '' )
-    ),
-    DoLog        = cms.bool( True ),
-    muonTag      = cms.InputTag( 'selectedPatMuons' ),
-    jetTag       = cms.InputTag( 'selectedPatJets' ),
-    electronTag  = cms.InputTag( 'selectedPatElectrons' ),
-    BtagName     = cms.string('jetProbabilityBJetTags'),
-    minMu1pt     = cms.double(20.),
-    minMu2pt     = cms.double(20.),
-    minJetPt     = cms.double(40),
-    maxMuAbsEta  = cms.double(2.4),
-    maxJetAbsEta = cms.double(2.5),
-    minMuMuMass  = cms.double(200),
-    min4objMass  = cms.double(520),
-    minMuonJetdR = cms.double(0.3), # (0.5)
-    muonTrackIsoLimitGeV = cms.double(10.0),
-
-    ZmassWinMinGeV= cms.double(84.),
-    ZmassWinMaxGeV= cms.double(98.),
-
-    applyJECUsign = cms.int32(0),
-
-    isSignal     = cms.bool(False),
-    mNuRnormalization = cms.double(1000.0)
-    )
+process.load("HeavyNu.AnalysisModules.heavynuanalysis_cfi")
 
 if isData:
     # turn on trigger match requirement
     process.hNu.trigMatchPset.trigEventTag=cms.InputTag("patTriggerEvent")
     process.hNu.trigMatchPset.muonMatch=cms.string('muonTriggerMatchHLTMuons')
+else:
+    # turn on MC trigger simulation
+    process.hNu.trigMatchPset.randomSeed=cms.int32(os.getpid())
 
 if Training:
     process.hNu.trainingFileName=cms.untracked.string("changeme_nntraining.txt")
@@ -198,14 +160,3 @@ if isMCsignal:
     process.hNu.isSignal = cms.bool(True)
 
 process.p += process.hNu
-
-# JEC uncertainty application
-# applyJECUsign=0 means don't apply it, which is the default
-#
-process.hNuJECUhi = process.hNu.clone(applyJECUsign = cms.int32(1))
-process.hNuJECUlo = process.hNu.clone(applyJECUsign = cms.int32(-1))
-
-process.pJECUhi = cms.Path(process.hNuJECUhi)
-process.pJECUlo = cms.Path(process.hNuJECUlo)
-
-process.s = cms.Schedule(process.p,process.pJECUhi,process.pJECUlo)
