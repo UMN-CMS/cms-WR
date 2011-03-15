8b57b941494d59eb24b718d8a2e611be99e49977
Phil Dudero <dudero@physics.umn.edu>
2011-03-15 20:59:53 +0000

    add MLL cut optimization, switch to log-likelihood fits, reorganize

--- AnalysisModules/test/opticut.C
+++ AnalysisModules/test/opticut.C
@@ -19,21 +19,31 @@
 #include "TStyle.h"
 #include "mystyle.C"
 
+#include "CLfast.C"
+
 using namespace std;
 
 static bool gl_verbose=true;
 
-const double zjet_scale = 1.57;
+//const double zjet_scale = 1.57;
 //const double zjet_scale = 1.46;
+const double zjet_scale = 1.15;
 //const double qcdp0 =  0.06514;  // constant
 //const double qcdp1 = -0.003923; // slope
-const double qcdp0 =  0.08354;    // constant
-const double qcdp1 = -0.009009;   // slope
+//const double qcdp0 =  0.08354;  // constant
+//const double qcdp1 = -0.009009; // slope
+const double qcdp0 =  0.1149;     // constant
+const double qcdp1 = -0.008046;   // slope
 
 const double optmwrcuts[] = {
-  760, 800, 880,1000,1080,1160,1240
+  //760, 800, 880,1000,1080,1160,1240
+  520,600,640,720,800,880,960,1040,1120,1200
 };
 const double binwidthGeV=40.0;
+
+//const int rebinMWRval = 5;
+const int rebinMWRval = 2;
+const int rebinMLLval = 1;
 
 //======================================================================
 
@@ -253,103 +263,291 @@ void fitndraw(wTH1 *wth1)
   wth1->SetStats(true,0,.6,.7,.99,.99);
   wth1->Draw("HIST");
   gPad->Update();
-  wth1->DrawFits("same");
+  wth1->DrawFits("same","LL");
   gPad->Update();
   wth1->DrawStats();
   gPad->Update();
 
   name += ".png";
-  //c1->SaveAs(name.c_str());
+  c1->SaveAs(name.c_str());
 }                                                            // fitndraw
 
 //======================================================================
 
-void scanCutVals(map<string,wTH1*>&  m_wth1,
-		 TF1 *ttfit,
-		 TF1 *zjfit,
-		 TF1 *wjfit,
-		 TF1 *vvfit,
-		 TF1 *qcdfit)
-{
-  printf("%-10s%10s%10s%10s%10s%10s%10s%10s%10s%10s%10s\n",
-"mWR(GeV)","mNU(GeV)","optcutgev","ttbar","zjets","wjets","VV","QCD","Sum BG","Sum Sig.","maxsignif");
+struct optVars_t {
+  optVars_t():
+    cutgev(0),ttint(0),zjint(0),wjint(0),vvint(0),twint(0),qcdint(0),
+    sumback(0),sumsig(0),sigeff(0),signif(0),clfactor(9e99) {}
+  double cutgev,ttint,zjint,wjint,vvint,twint,qcdint,sumback,sumsig,sigeff,signif,clfactor;
+};
 
-  for (double mwr=1000; mwr<=1600; mwr+=100) {
-    cout<<"=========================================================";
-    cout<<"========================================================="<<endl;
+struct optFits_t {
+  optFits_t():tt(0),zj(0),wj(0),vv(0),tw(0),qcd(0) {}
+  TF1 *tt,*zj,*wj,*vv,*tw,*qcd;
+};
+
+//======================================================================
+
+void loadMWRFits(map<string,wTH1*>&  m_wth1,
+		 optFits_t& fits)
+{
+  wTH1 *ttbar = m_wth1["ttjets_m4"];
+  wTH1 *zjets = m_wth1["zjets_m4"];
+  wTH1 *wjets = m_wth1["wjets_m4"];
+  wTH1 *vv    = m_wth1["vv_m4"];
+  wTH1 *tw    = m_wth1["tw_m4"];
+
+  fits.tt = new TF1("ttfit","expo",650,2000);
+  fits.zj = new TF1("zjfit","expo",650,2000);
+  fits.wj = new TF1("wjfit","expo",560,2000); // note!
+  fits.vv = new TF1("vvfit","expo",650,2000);
+  fits.tw = new TF1("twfit","expo",650,2000);
+
+  ttbar->loadFitFunction(fits.tt);
+  zjets->loadFitFunction(fits.zj);
+  wjets->loadFitFunction(fits.wj);
+  vv   ->loadFitFunction(fits.vv);
+  tw   ->loadFitFunction(fits.tw);
+
+  ttbar->histo()->Rebin(rebinMWRval);
+  vv   ->histo()->Rebin(rebinMWRval);
+  tw   ->histo()->Rebin(rebinMWRval);
+
+  // Scale zjets according to Z-peak fit:
+  zjets->histo()->Scale(zjet_scale);
+  zjets->histo()->Rebin(rebinMWRval);
+
+  fits.qcd = new TF1("qcdfit","expo",650,2000);
+  fits.qcd->SetParameter(0,qcdp0);
+  fits.qcd->SetParameter(1,qcdp1);
+
+  fitndraw(wjets);
+  fitndraw(vv);
+  fitndraw(tw);
+  fitndraw(zjets);
+  fitndraw(ttbar);
+
+  printf("%-9s%10s%10s%10s\n", "Sample","p0","p1", "chi2/ndof");
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","TTJets",
+	 fits.tt->GetParameter(0),fits.tt->GetParameter(1),
+	 fits.tt->GetChisquare(),fits.tt->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","ZJets",
+	 fits.zj->GetParameter(0),fits.zj->GetParameter(1),
+	 fits.zj->GetChisquare(),fits.zj->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","VV",
+	 fits.vv->GetParameter(0),fits.vv->GetParameter(1),
+	 fits.vv->GetChisquare(),fits.vv->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","TW",
+	 fits.tw->GetParameter(0),fits.tw->GetParameter(1),
+	 fits.tw->GetChisquare(),fits.tw->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","WJets",
+	 fits.wj->GetParameter(0),fits.wj->GetParameter(1),
+	 fits.wj->GetChisquare(),fits.wj->GetNDF());
+
+}                                                         // loadMWRFits
+
+//======================================================================
+
+void loadMLLFits(map<string,wTH1*>&  m_wth1,
+		 optFits_t& fits)
+{
+  wTH1 *ttbar = m_wth1["ttjets_m2"];
+  wTH1 *zjets = m_wth1["zjets_m2"];
+  wTH1 *wjets = m_wth1["wjets_m2"];
+  wTH1 *vv    = m_wth1["vv_m2"];
+  wTH1 *tw    = m_wth1["tw_m2"];
+
+  fits.tt = new TF1("ttfit","expo",160,1000);
+  fits.zj = new TF1("zjfit","expo",160,2000);
+  fits.wj = new TF1("wjfit","expo",120,300); // note!
+  fits.vv = new TF1("vvfit","expo",160,1200);
+  fits.tw = new TF1("twfit","expo",160,1000);
+
+  ttbar->loadFitFunction(fits.tt);
+  zjets->loadFitFunction(fits.zj);
+  wjets->loadFitFunction(fits.wj);
+  vv   ->loadFitFunction(fits.vv);
+  tw   ->loadFitFunction(fits.tw);
+
+  ttbar->histo()->Rebin(rebinMLLval);
+  vv   ->histo()->Rebin(rebinMLLval);
+  tw   ->histo()->Rebin(rebinMLLval);
+
+  // Scale zjets according to Z-peak fit:
+  zjets->histo()->Scale(zjet_scale);
+  zjets->histo()->Rebin(rebinMLLval);
+
+  fitndraw(wjets);
+  fitndraw(vv);
+  fitndraw(tw);
+  fitndraw(zjets);
+  fitndraw(ttbar);
+
+  printf("%-9s%10s%10s%10s\n", "Sample","p0","p1", "chi2/ndof");
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","TTJets",
+	 fits.tt->GetParameter(0),fits.tt->GetParameter(1),
+	 fits.tt->GetChisquare(),fits.tt->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","ZJets",
+	 fits.zj->GetParameter(0),fits.zj->GetParameter(1),
+	 fits.zj->GetChisquare(),fits.zj->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","VV",
+	 fits.vv->GetParameter(0),fits.vv->GetParameter(1),
+	 fits.vv->GetChisquare(),fits.vv->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","TW",
+	 fits.tw->GetParameter(0),fits.tw->GetParameter(1),
+	 fits.tw->GetChisquare(),fits.tw->GetNDF());
+  printf("%-9s%10.4f%10.4f%10.4g/%d\n","WJets",
+	 fits.wj->GetParameter(0),fits.wj->GetParameter(1),
+	 fits.wj->GetChisquare(),fits.wj->GetNDF());
+
+}                                                         // loadMLLFits
+
+//======================================================================
+
+void calcVars(TH1 *sighist,const optFits_t& fits,optVars_t& vars,double rebinval)
+{
+  int lobin    = sighist->FindBin(vars.cutgev);
+  int hibin    = sighist->FindBin(1+sighist->GetNbinsX());
+  vars.sumsig  = sighist->Integral(lobin,hibin);
+  vars.sigeff  = sighist->GetEntries()/10000.;
+
+  vars.ttint   = fits.tt ->Integral(vars.cutgev,2000)/(binwidthGeV*rebinval);
+  vars.zjint   = fits.zj ->Integral(vars.cutgev,2000)/(binwidthGeV*rebinval);
+  vars.wjint   = fits.wj ->Integral(vars.cutgev,2000)/binwidthGeV;
+  vars.vvint   = fits.vv ->Integral(vars.cutgev,2000)/(binwidthGeV*rebinval);
+  vars.twint   = fits.tw ->Integral(vars.cutgev,2000)/(binwidthGeV*rebinval);
+  if (fits.qcd)
+    vars.qcdint= fits.qcd->Integral(vars.cutgev,2000)/binwidthGeV;
+	
+  vars.sumback = vars.ttint+vars.zjint+vars.wjint+vars.vvint+vars.twint+vars.qcdint;
+
+  vars.signif = vars.sumsig/sqrt(vars.sumsig+vars.sumback);
+
+  vars.clfactor = CLfast_goalSeek(vars.sumsig,vars.sumback);
+}                                                            // calcVars
+
+//======================================================================
+
+inline
+void putvars(const char *fmt, double mwr, double mnu, const optVars_t& v)
+{
+  printf(fmt,mwr,mnu,v.cutgev,v.ttint,v.zjint,v.wjint,v.vvint,v.twint,
+	 v.qcdint,v.sumback,v.sumsig,v.signif,v.clfactor);
+}                                                            // putvars
+
+//======================================================================
+
+void scanMWRCutVals(map<string,wTH1*>&  m_wth1,
+		    const optFits_t& fits)
+{
+  for (double mwr=700; mwr<=1600; mwr+=100) {
+
+    cout<<"=================================================================";
+    cout<<"================================================================="<<endl;
+
+    printf("%-9s%9s%10s%10s%10s%10s%10s%10s%10s%10s%10s%10s%10s\n",
+"mWR(GeV)","mNU(GeV)","optcutgev","ttbar","zjets","wjets","VV","tW","QCD","Sum BG","Sum Sig.","signif","clfactor");
+
+    cout<<"=================================================================";
+    cout<<"================================================================="<<endl;
+
     for (double mnu=100; mnu<mwr; mnu+=100) {
+      optVars_t minclfact;
+      optVars_t maxsignif;
+      optVars_t vars;
+
+      // get signal histo for this masspoint from the input map
       char s[80];
       sprintf( s, "WR%.0f_nuRmu%.0f",mwr,mnu );
       string name(s);
 
-      if (!m_wth1[name]) continue;
+      if (!m_wth1[name]) {
+	cerr << "Didn't find " << name << endl;
+	continue;
+      }
 
       TH1 *sighist = m_wth1[name]->histo();
 
-      double maxsignif=0;
-      double optcutgev=0;
-      double optttint   = 0;
-      double optzjint   = 0;
-      double optwjint   = 0;
-      double optvvint   = 0;
-      double optqcdint  = 0;
-      double optsumback = 0;
-      double optsumsig  = 0;
+      for (vars.cutgev=520; vars.cutgev<mwr; vars.cutgev+=binwidthGeV) {
+	calcVars(sighist,fits,vars,rebinMWRval);
+	if (vars.signif   > maxsignif.signif)   maxsignif = vars;
+	if (vars.clfactor < minclfact.clfactor) minclfact = vars;
+      }
 
-      for (double cutgev=600; cutgev<mwr; cutgev+=binwidthGeV) {
-	double ttint   =  ttfit->Integral(cutgev,2000)/binwidthGeV;
-	double zjint   =  zjfit->Integral(cutgev,2000)/(binwidthGeV*2);
-	double wjint   =  wjfit->Integral(cutgev,2000)/binwidthGeV;
-	double vvint   =  vvfit->Integral(cutgev,2000)/binwidthGeV;
-	double qcdint  = qcdfit->Integral(cutgev,2000)/binwidthGeV;
-	
-	double sumback = ttint+zjint+wjint+vvint+qcdint;
-
-	int lobin = sighist->FindBin(cutgev);
-	int hibin = sighist->FindBin(2000);
-      
-	double sumsig = sighist->Integral(lobin,hibin-1);
-	double signif = sumsig/sqrt(sumsig+sumback);
-
-	if (signif > maxsignif) {
-	  maxsignif = signif;
-	  optcutgev  = cutgev;
-	  optttint  = ttint;
-	  optzjint  = zjint;
-	  optwjint  = wjint;
-	  optvvint  = vvint;
-	  optqcdint = qcdint;
-	  optsumback= sumback;
-	  optsumsig = sumsig;
-	}
-      } // cutgev loop
-
-      printf("%-10.0f%10.0f%10.0f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f\n",
-mwr,mnu,optcutgev,optttint,optzjint,optwjint,optvvint,optqcdint,optsumback,optsumsig,maxsignif);
-
+      putvars("%-9.0f%9.0f%10.0f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f(*)%7.3f\n",
+	      mwr,mnu,maxsignif);
+      putvars("%-9.0f%9.0f%10.0f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f(*)\n\n",
+	      mwr,mnu,minclfact);
     } // mnu loop
   } // mwr loop
-}                                                         // scanCutVals
+}                                                      // scanMWRCutVals
+
+//======================================================================
+// almost the same as above, but different cut optimization range
+//
+void scanMLLCutVals(map<string,wTH1*>&  m_wth1,
+		    const optFits_t& fits)
+{
+  for (double mwr=1000; mwr<=1600; mwr+=100) {
+
+    cout<<"=================================================================";
+    cout<<"================================================================="<<endl;
+
+    printf("%-9s%9s%10s%10s%10s%10s%10s%10s%10s%10s%10s%10s%10s\n",
+"mWR(GeV)","mNU(GeV)","optcutgev","ttbar","zjets","wjets","VV","tW","QCD","Sum BG","Sum Sig.","signif","clfactor");
+
+    cout<<"=================================================================";
+    cout<<"================================================================="<<endl;
+
+    for (double mnu=100; mnu<mwr; mnu+=100) {
+      optVars_t minclfact;
+      optVars_t maxsignif;
+      optVars_t vars;
+
+      // get signal histo for this masspoint from the input map
+      char s[80];
+      sprintf( s, "WR%.0f_nuRmu%.0f",mwr,mnu );
+      string name(s);
+
+      if (!m_wth1[name]) {
+	cerr << "Didn't find " << name << endl;
+	continue;
+      }
+
+      TH1 *sighist = m_wth1[name]->histo();
+
+      for (vars.cutgev=120; vars.cutgev<760; vars.cutgev+=binwidthGeV) {
+	calcVars(sighist,fits,vars,rebinMLLval);
+	if (vars.signif   > maxsignif.signif)   maxsignif = vars;
+	if (vars.clfactor < minclfact.clfactor) minclfact = vars;
+      }
+
+      putvars("%-9.0f%9.0f%10.0f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f(*)%7.3f\n",
+	      mwr,mnu,maxsignif);
+      putvars("%-9.0f%9.0f%10.0f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f%10.3f(*)\n\n",
+	      mwr,mnu,minclfact);
+    } // mnu loop
+  } // mwr loop
+}                                                      // scanMLLCutVals
 
 //======================================================================
 
 void printDanilaTable(map<string,wTH1*>&  m_wth1,
-		      TF1 *ttfit,
-		      TF1 *zjfit,
-		      TF1 *wjfit,
-		      TF1 *vvfit,
-		      TF1 *qcdfit)
+		      const optFits_t& fits)
 {
   printf("MWR\tMNu\tL1Pt\tL2Pt\tL1Eta\tL2Eta\tMll\tMwr_cut\t");
-  printf("S_nw\tS_eff\tBG_nw\tQCD_nw\tTTb_nw\tZJ_nw\tWJ_nw\tVV_nw\n");
+  printf("S_nw\tS_eff\tBG_nw\tQCD_nw\tTTb_nw\tZJ_nw\tWJ_nw\tVV_nw\ttW_nw\n");
 
-  for (int i=0; i<7; i++) {
-    double mwr = 1000. + i*100.;
-    double optmwrcut = optmwrcuts[i];
+  for (int i=0; i<10; i++) {
+    double mwr = 700. + i*100.;
+    optVars_t v;
+    v.cutgev = optmwrcuts[i];
+
     cout<<"==============================================================";
     cout<<"=============================================================="<<endl;
 
     for (double mnu=100; mnu<mwr; mnu+=100) {
+      // get signal histo for this masspoint from the input map
       char s[80];
       sprintf( s, "WR%.0f_nuRmu%.0f",mwr,mnu );
       string name(s);
@@ -358,40 +556,28 @@ void printDanilaTable(map<string,wTH1*>&  m_wth1,
 
       TH1 *sighist   = m_wth1[name]->histo();
 
-      double sigeff  = sighist->GetEntries()/10000.;
+      calcVars(sighist,fits,v,rebinMWRval);
 
-      double ttint   =  ttfit->Integral(optmwrcut,2000)/binwidthGeV;
-      double zjint   =  zjfit->Integral(optmwrcut,2000)/(binwidthGeV*2);
-      double wjint   =  wjfit->Integral(optmwrcut,2000)/binwidthGeV;
-      double vvint   =  vvfit->Integral(optmwrcut,2000)/binwidthGeV;
-      double qcdint  = qcdfit->Integral(optmwrcut,2000)/binwidthGeV;
-	
-      double sumback = ttint+zjint+wjint+vvint+qcdint;
-
-      int lobin = sighist->FindBin(optmwrcut);
-      int hibin = sighist->FindBin(1+sighist->GetNbinsX());
-      
-      double sumsig = sighist->Integral(lobin,hibin);
-
-      printf("%4.0f\t%4.0f\t60.0\t20.0\t2.1\t2.5\t200\t%4.0f\t",mwr,mnu,optmwrcut);
-      printf("%6.3f\t%5.3f\t%6.3f\t%6.4f\t%6.3f\t%6.3f\t%6.4f\t%6.4f\n",
-	     sumsig,sigeff,sumback,qcdint,ttint,zjint,wjint,vvint);
+      printf("%4.0f\t%4.0f\t60.0\t20.0\t2.1\t2.4\t200\t%4.0f\t",mwr,mnu,v.cutgev);
+      printf("%6.3f\t%5.3f\t%6.3f\t%6.4f\t%6.3f\t%6.3f\t%6.4f\t%6.4f\t%6.4f\n",
+	     v.sumsig,v.sigeff,v.sumback,v.qcdint,v.ttint,v.zjint,v.wjint,v.vvint,v.twint);
 
     } // mnu loop
   } // mwr loop
-}                                                // printDanilaTable
+}                                                    // printDanilaTable
 
 //======================================================================
 
 void opticut()
 {
   map<string,wTH1 *> m_wth1;
+  optFits_t fits;
 
   setPRDStyle();
 
   // preload map so I don't have to do finds and inserts
   //
-  for (double mwr=1000; mwr<=1600; mwr+=1000) {
+  for (double mwr=700; mwr<=1600; mwr+=1000) {
     for (double mnu=100; mnu<mwr; mnu+=100) {
       char s[80];
       sprintf( s, "WR%.0f_nuRmu%.0f",mwr,mnu );
@@ -400,35 +586,14 @@ void opticut()
     }
   }
 
-  getHistosFromRE("optim.root",".*",m_wth1);
-
-  wTH1 *ttbar = m_wth1["ttbar_m4"];
-  wTH1 *zjets = m_wth1["zjets_m4"];
-  wTH1 *wjets = m_wth1["wjets_m4"];
-  wTH1 *vv    = m_wth1["vv_m4"];
-
-  TF1 *ttfit = new TF1("ttfit","expo",650,2000);
-  TF1 *zjfit = new TF1("zjfit","expo",650,2000);
-  TF1 *wjfit = new TF1("wjfit","expo",650,2000);
-  TF1 *vvfit = new TF1("vvfit","expo",650,2000);
-  ttbar->loadFitFunction(ttfit);
-  zjets->loadFitFunction(zjfit);
-  wjets->loadFitFunction(wjfit);
-  vv   ->loadFitFunction(vvfit);
-
-  // Scale zjets according to Z-peak fit:
-  //zjets->histo()->Scale(zjet_scale);
-  zjets->histo()->Rebin(2);
-
-  TF1 *qcdfit = new TF1("qcdfit","expo",650,2000);
-  qcdfit->SetParameter(0,qcdp0);
-  qcdfit->SetParameter(1,qcdp1);
-
-  fitndraw(wjets);
-  fitndraw(vv);
-  fitndraw(zjets);
-  fitndraw(ttbar);
-  //scanCutVals(m_wth1,ttfit,zjfit,wjfit,vvfit,qcdfit);
-
-  printDanilaTable(m_wth1,ttfit,zjfit,wjfit,vvfit,qcdfit);
+#if 0
+  getHistosFromRE("optimMLL.root",".*",m_wth1);
+  loadMLLFits(m_wth1,fits);
+  scanMLLCutVals(m_wth1,fits);
+#else
+  getHistosFromRE("optimMWR.root",".*",m_wth1);
+  loadMWRFits(m_wth1,fits);
+  scanMWRCutVals(m_wth1,fits);
+  //printDanilaTable(m_wth1,fits);
+#endif
 }
