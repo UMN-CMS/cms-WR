5140bbda9b79808a093ab96e12e7ec3969391847
Phil Dudero <dudero@physics.umn.edu>
2011-03-15 01:24:35 +0000

    first commit for cut optimization

---
+++ AnalysisModules/test/hnuRelaxedMLLcut_cfg.py
@@ -0,0 +1,145 @@
+import FWCore.ParameterSet.Config as cms
+
+import os
+
+#import sys
+#isMC=sys.modules['__main__'].isMC
+#isMCsignal=sys.modules['__main__'].isMCsignal
+#process = sys.modules['__main__'].process
+
+isMC=True
+isRun2010LoLumi=False
+
+isData=not isMC
+
+## Low and high lumi data selection is controlled by the JSON-derived cfi's imported
+## below. For run 2010, the low lumi data is that for which the HLT_Mu9 trigger path
+## was active and unprescaled, (uncertified) run range 133446 - 147116. Certification
+## restricts this run range further.
+##
+isRun2010HiLumi=not isRun2010LoLumi
+
+process = cms.Process("PAT");
+
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound'),
+    wantSummary = cms.untracked.bool(True)
+)
+# source
+#process.source = cms.Source("PoolSource",
+#    fileNames=cms.untracked.vstring('file:/hdfs/cms/skim/mu/39X/Dec22ReReco/Run2010B/Mu_Run2010B_Dec22ReReco_v1_AOD_068-muSkim-pool.root')
+#This file is AT FNAL:
+#    fileNames=cms.untracked.vstring('/store/mc/Fall10/Z1Jets_ptZ-0to100_TuneZ2_7TeV-alpgen-tauola/GEN-SIM-RECO/START38_V12-v2/0018/027050CA-D50B-E011-91DD-00261894391C.root')
+#    fileNames=cms.untracked.vstring( "file:/hdfs/cms/user/heavynu/HeavyNuRecoFromHLT/38X/WR1000_nuRmu100/HeavyNuGenHLT_WR1000_nuRmu100_1-reco-pool.root" )
+#)
+process.load('HeavyNu.AnalysisModules.in_cff')
+
+if isData:
+    if isRun2010LoLumi:
+        print "===========> Flag is SET for LOW luminosity data <============"
+        from HeavyNu.AnalysisModules.run2010loLumiRunList_cfi import lumisToProcess
+    else:
+        print "===========> Flag is SET for HIGH luminosity data <============"
+        from HeavyNu.AnalysisModules.run2010hiLumiRunList_cfi import lumisToProcess
+    
+    process.source.lumisToProcess = lumisToProcess
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+## Load additional processes
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+## global tags:
+if (isMC):
+    print "=================> MC flag is SET <===================="
+    process.GlobalTag.globaltag = cms.string('START38_V14::All')
+else:
+    process.GlobalTag.globaltag = cms.string('GR_R_38X_V15::All')
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+################################################################################################
+###    P r e p a r a t i o n      o f    t h e    P A T    O b j e c t s   f r o m    A O D  ###
+################################################################################################
+
+## pat sequences to be loaded:
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
+
+## ---
+## Define the path
+## ---
+process.p = cms.Path(
+  process.patDefaultSequence
+)
+
+if isData:
+    from PhysicsTools.PatAlgos.tools.coreTools import *
+    removeMCMatching( process, ['All'], outputInProcess=False )
+
+########################################
+# PAT Jet Energy Corrections - MC vs Data
+########################################
+# 
+
+from PhysicsTools.PatAlgos.tools.jetTools import switchJetCollection
+if isMC:
+    switchJetCollection( process,
+                         jetCollection=cms.InputTag('ak5CaloJets'),
+                         jetCorrLabel=('AK5Calo', ['L2Relative','L3Absolute']))
+else:
+    switchJetCollection( process,
+                         jetCollection=cms.InputTag('ak5CaloJets'),
+                         jetCorrLabel=('AK5Calo', ['L2Relative','L3Absolute','L2L3Residual']))
+
+########################################
+# PAT Trigger matching
+########################################
+# imported directly from PhysicsTools/PatExamples/test/analyzePatTrigger_onTheFly_cfg.py
+#
+process.load("HeavyNu.AnalysisModules.hnutrigmatch_cfi")
+
+### ============
+### Python tools
+### ============
+### Attention: order matters!
+
+## --
+## Switch to selected PAT objects in the main work flow
+## --
+from PhysicsTools.PatAlgos.tools.coreTools import removeCleaning
+removeCleaning( process, False )
+
+## --
+## Switch on PAT trigger - but only for data!
+## --
+from PhysicsTools.PatAlgos.tools.trigTools import *
+if isData:
+    switchOnTriggerMatching( process, triggerMatchers = [ 'muonTriggerMatchHLTMuons' ], outputModule='' )
+    removeCleaningFromTriggerMatching( process, outputModule='' )
+    if isRun2010LoLumi: process.muonTriggerMatchHLTMuons.pathNames = cms.vstring('HLT_Mu9')
+    else:               process.muonTriggerMatchHLTMuons.pathNames = cms.vstring('HLT_Mu15_v1')
+
+##########################################
+## Add analysis
+##########################################
+
+process.TFileService = cms.Service("TFileService",
+       fileName = cms.string("anal.root"),
+)
+
+process.load("HeavyNu.AnalysisModules.heavynuanalysis_cfi")
+process.hNu.minMuMuMass  = cms.double(120)
+process.hNu.min4objMass  = cms.double(760)
+
+if isData:
+    # turn on trigger match requirement
+    process.hNu.trigMatchPset.trigEventTag=cms.InputTag("patTriggerEvent")
+    process.hNu.trigMatchPset.muonMatch=cms.string('muonTriggerMatchHLTMuons')
+else:
+    # turn on MC trigger simulation
+    process.hNu.trigMatchPset.randomSeed=cms.int32(os.getpid())
+
+process.p += process.hNu
