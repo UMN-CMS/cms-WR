aa0108db5662edada78ebf9a1de9c2032972ee25
Jeremiah Mans <jmmans@physics.umn.edu>
2011-12-20 15:01:49 +0000

    forgetting the past

---
+++ Limits/ctool/makeLimitFileMain.cc
@@ -0,0 +1,31 @@
+#include "makeLimitFile.hh"
+#include "systematics.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "TFile.h"
+
+int main(int argc, char* argv[]) {
+  if (argc<9) {
+    printf("Usage: makeLimitFile [lumi] [mwr] [mn] [xsec] [data file] [signal file] [outfilename] [systematicsdb] [optional special syst mode]\n");
+    return 1;
+  }
+  float lumi=atof(argv[1]);
+  MassPoint pt;
+  pt.mwr=atoi(argv[2]);
+  pt.mnr=atoi(argv[3]);
+  float xsec=atof(argv[4]);
+  TFile df(argv[5]);
+  TFile sf(argv[6]);
+
+  SystematicsDB syst;
+  syst.load(argv[8]);
+
+  if (argc==10)
+    int special_syst_mode=atoi(argv[9]);
+
+  
+
+  makeLimitFile(lumi,xsec,pt,&df,&sf,argv[7],syst);
+
+  return 0;
+}
