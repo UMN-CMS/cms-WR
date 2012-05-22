80ca8f03dae57c0366d86df144296254bfb9d54e
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2012-05-22 21:01:30 +0000

    fat so: co la pudeta al char de sumelek

---
+++ AnalysisModules/python/heavyNuEleTriggerEff_cfi.py
@@ -0,0 +1,70 @@
+import FWCore.ParameterSet.Config as cms
+
+HeavyNuEleTriggerEff = cms.EDAnalyzer(
+    'HeavyNuEleTriggerEff',
+    
+    trigEventTag     = cms.InputTag("patTriggerEvent"),
+    doDebugMessages  = cms.bool(False),
+    #doDebugMessages  = cms.bool(True),
+    
+    # to implement HEEP ID
+    rhoForHEEPId      = cms.InputTag("kt6PFJetsForIsolation","rho","triggerEfficienciesEle"),
+
+    electronTag  = cms.InputTag("patElectrons"),
+    maxAbsEtaOfflEle = cms.double( 2.5 ),
+    minPtOfflEle     = cms.double(  40 ),
+    heepVersion      = cms.int32(   40 ),
+    ebScale          = cms.double(   1. ),
+    eeScale          = cms.double(   1. ),
+
+    #jetTag           = cms.InputTag("patJets","pfCandidates",""),
+    jetTag           = cms.InputTag("cleanPatJets","",""),
+    numOfflJets      = cms.int32(    2 ),
+    minPtOfflJets    = cms.double(   30),
+
+
+    
+    seedHLTForL1Efficiency =  cms.vstring(
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v3',
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v4',
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v5'
+    ),
+
+    targetL1Algo          = cms.string('L1_SingleEG22'),
+
+    nonIsolatedEmSource = cms.InputTag("l1extraParticles","NonIsolated"),
+    isolatedEmSource    = cms.InputTag("l1extraParticles","Isolated"),
+    
+    seedHLTForHLTEfficiency =  cms.vstring(
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v3',
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v4',
+    'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_v5'
+    ),
+    minPtOfObjects = cms.double( 33. ),
+
+    targetHLTPaths          = cms.vstring(
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v2',
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v3',
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v4', # this seems to be the matchging one
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v5',
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v6',
+    'HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_v7',
+    ),
+
+    #electronFilters = cms.vstring('hltDiEle33CaloIdLGsfTrkIdVLDPhiDoubleFilter')
+    # the last filter of 'HLT_Ele32_CaloIdT_CaloIsoT_TrkIdT_TrkIsoT_SC17_Mass50_vx'
+    #electronSeedingFilters = cms.vstring('HLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17Mass50Sequence'),
+    #electronSeedingFilters = cms.vstring('HLTEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17Mass50Sequence'),
+
+
+    electronSeedingFilters = cms.vstring('hltEle32CaloIdTCaloIsoTTrkIdTTrkIsoTSC17PMMassFilter'),
+    # documentation of this choice partly addressed here: 
+    # https://hypernews.cern.ch/HyperNews/CMS/get/egamma-hlt/168/1.html
+
+    
+    electronTargetFilters  = cms.vstring('hltDiEle33CaloIdLGsfTrkIdVLDPhiDoubleFilter'),
+  
+    
+    plotFolderName  = cms.string('twoJets'),
+    
+    )
