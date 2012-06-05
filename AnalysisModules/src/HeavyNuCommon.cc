21ada32cdd2c7f42980e1fbd1ba7760805d00f78
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2012-06-05 21:56:32 +0000

    import tools to get objects passing trigger filters (from S. Harper)

--- AnalysisModules/src/HeavyNuCommon.cc
+++ AnalysisModules/src/HeavyNuCommon.cc
@@ -907,3 +907,50 @@ namespace hnu {
   }
 
 }
+
+
+
+
+
+
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+
+void trigtools::getP4sOfObsPassingFilter(std::vector<math::XYZTLorentzVector>& p4s,const trigger::TriggerEvent& trigEvent,const std::string& filterName,const std::string& hltProcess)
+{
+  p4s.clear();
+
+  edm::InputTag filterTag(filterName,"",hltProcess); 
+  trigger::size_type filterIndex = trigEvent.filterIndex(filterTag); 
+  if(filterIndex<trigEvent.sizeFilters()){ //check that filter is in triggerEvent
+    const trigger::Keys& trigKeys = trigEvent.filterKeys(filterIndex); 
+    const trigger::TriggerObjectCollection & trigObjColl(trigEvent.getObjects());
+    for(trigger::Keys::const_iterator keyIt=trigKeys.begin();keyIt!=trigKeys.end();++keyIt){ 
+      const trigger::TriggerObject& obj = trigObjColl[*keyIt];
+      math::XYZTLorentzVector objP4;
+      objP4.SetPxPyPzE(obj.px(),obj.py(),obj.pz(),obj.energy());
+      p4s.push_back(objP4);
+    }//end loop over keys
+  }//end check that filter is valid and in trigEvent
+}
+
+void trigtools::getP4sOfObsPassingFilter(std::vector<TLorentzVector>& p4s,const trigger::TriggerEvent& trigEvent,const std::string& filterName,const std::string& hltProcess)
+{
+  p4s.clear();
+ 
+  edm::InputTag filterTag(filterName,"",hltProcess); 
+  trigger::size_type filterIndex = trigEvent.filterIndex(filterTag); 
+  if(filterIndex<trigEvent.sizeFilters()){ //check that filter is in triggerEvent
+    const trigger::Keys& trigKeys = trigEvent.filterKeys(filterIndex); 
+    const trigger::TriggerObjectCollection & trigObjColl(trigEvent.getObjects());
+    for(trigger::Keys::const_iterator keyIt=trigKeys.begin();keyIt!=trigKeys.end();++keyIt){ 
+      const trigger::TriggerObject& obj = trigObjColl[*keyIt];
+      TLorentzVector objP4;
+      objP4.SetPtEtaPhiM(obj.pt(),obj.eta(),obj.phi(),obj.mass());
+      p4s.push_back(objP4);
+    }//end loop over keys
+  }//end check that filter is valid and in trigEvent
+}
+
+
--- AnalysisModules/src/HeavyNuCommon.h
+++ AnalysisModules/src/HeavyNuCommon.h
@@ -173,3 +173,39 @@ namespace hnu {
 
 
 #endif // HEAVY_NU_COMMON_INCLUDED
+
+
+
+
+#ifndef HNU_TRIGTOOLSFUNCS
+#define HNU_TRIGTOOLSFUNCS
+
+#include "DataFormats/Math/interface/LorentzVector.h"
+
+#include "TLorentzVector.h"
+
+#include<vector>
+#include<string>
+
+
+// code initially imported from Sam Harpers' (RAL) personal work area:
+// http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/SHarper/TrigTools/interface/TrigToolsFuncs.h?revision=1.1&view=markup&pathrev=HEAD
+
+namespace trigger{
+  class TriggerEvent;
+}
+
+namespace trigtools {
+  
+  //this function takes the trigger event, a filtername + hlt process name (usally HLT but different if 
+  //HLT was re-run) and returns a vector of the four-momenta of all objects passing the filter
+  void getP4sOfObsPassingFilter(std::vector<math::XYZTLorentzVector>& p4s,const trigger::TriggerEvent& trigEvent,const std::string& filterName,const std::string& hltProcess="HLT");
+
+  //a TLorentzVector version for my ntuplising needs
+  void getP4sOfObsPassingFilter(std::vector<TLorentzVector>& p4s,const trigger::TriggerEvent& trigEvent,const std::string& filterName,const std::string& hltProcess="HLT");
+  
+  
+
+}
+
+#endif
