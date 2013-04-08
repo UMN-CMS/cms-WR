fc6884bb127187f868cbda0db87096d9ed5a9228
Joe Pastika <pastika@physics.umn.edu>
2013-04-08 23:24:03 +0000

    Updated to newest version

--- AnalysisModules/src/HeavyNuTree.h
+++ AnalysisModules/src/HeavyNuTree.h
@@ -8,43 +8,61 @@
 class HeavyNuTree
 {
 private:
-    TDirectory& m_file;
-    TTree* m_tree;
-    TBranch* branch_;
+   TDirectory& m_file;
+   TTree* m_tree;
+   TBranch* branch_;
+   int nevt;
 
 public:
 
-    struct HNuSlopeFitInfo
-    {
-        Float_t mlljj, mll, weight, l1pt, l2pt;
-        Short_t flavor, cutlevel, n_pileup, n_primaryVertex;
-    } event_;
+   struct HNuSlopeFitInfo
+   {
+      Float_t mlljj, mll, weight, l1pt, l2pt;
+      Short_t flavor, cutlevel, n_pileup, n_primaryVertex;
+   } event_;
 
-    HeavyNuTree(TDirectory& f, bool writable) : m_file(f)
-    {
-        if(writable)
-        {
-            m_file.cd();
-            m_tree = new TTree("HeavyNuTuple", "HeavyNuTuple");
-            branch_ = m_tree->Branch("slopefit", &event_, "mlljj/F:mll:weight:l1pt:l2pt:flavor/S:cutlevel:npu:npv");
-        }
-        else
-        {
-            m_tree = (TTree*) m_file.Get("HeavyNuTuple");
-            m_tree->SetBranchAddress("slopefit", &event_);
-        }
-    }
+   HeavyNuTree(TDirectory& f, bool writable) : m_file(f)
+   {
+      nevt = 0;
+      if(writable)
+      {
+         m_file.cd();
+         m_tree = new TTree("HeavyNuTuple", "HeavyNuTuple");
+         branch_ = m_tree->Branch("slopefit", &event_, "mlljj/F:mll:weight:l1pt:l2pt:flavor/S:cutlevel:npu:npv");
+      }
+      else
+      {
+         m_tree = (TTree*) m_file.Get("HeavyNuTuple");
+         m_tree->SetBranchAddress("slopefit", &event_);
+      }
+   }
 
-    void clear()
-    {
-        event_.mlljj = event_.mll = event_.weight = 0.0;
-        event_.flavor = event_.cutlevel = event_.n_pileup = event_.n_primaryVertex = 0;
-    }
+   int Entries()
+   {
+      return m_tree->GetEntries();
+   }
 
-    void fill()
-    {
-        m_tree->Fill();
-    }
+   bool GetNextEvent()
+   {
+      if(nevt == Entries())
+      {
+         return false;
+      }
+      m_tree->GetEntry(nevt);
+      nevt++;
+      return true;
+   }
+
+   void clear()
+   {
+      event_.mlljj = event_.mll = event_.weight = 0.0;
+      event_.flavor = event_.cutlevel = event_.n_pileup = event_.n_primaryVertex = 0;
+   }
+
+   void fill()
+   {
+      m_tree->Fill();
+   }
 };
 
 #endif
