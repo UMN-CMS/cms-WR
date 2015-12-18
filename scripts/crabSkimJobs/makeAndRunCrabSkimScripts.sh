15aed899c5e2f1d9c73fefba7358ec29dd7045fe
Shervin <shervin@cern.ch>
2015-12-18 08:05:41 +0100

    putting configurations in a separate file called 2015-v1.conf

--- scripts/crabSkimJobs/makeAndRunCrabSkimScripts.sh
+++ scripts/crabSkimJobs/makeAndRunCrabSkimScripts.sh
@@ -2,11 +2,15 @@
 ### this script should be done in python!!!! to be well integrated with the crab submission
 #source /cvmfs/cms.cern.ch/crab3/crab.sh
 
-datasetFile=configs/datasets.dat
-jsonFile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt
-jsonName=246908-260627-Prompt_25ns-golden_silver-v1
-crabFile=tmp/crab.py
 
+###### the following variables are defined in a separate config file
+# datasetFile=configs/datasets.dat
+# jsonFile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt
+# jsonName=246908-260627-Prompt_25ns-golden_silver-v1
+source configs/2015-v1.conf
+
+
+crabFile=tmp/crab.py
 
 datasets=(`cat $datasetFile | grep -v '#' | awk '{print $1}'`)
 datasetNames=(`cat $datasetFile | grep -v '#' | awk '(NF==2 || NF>=4){print $2}'`)
