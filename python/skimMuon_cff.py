8d592d6be9f96b70a8888df1a6e2c5d2652cb9aa
Shervin <shervin@cern.ch>
2015-12-18 10:19:46 +0100

    fixing few names in the skim sequence

--- python/skimMuon_cff.py
+++ python/skimMuon_cff.py
@@ -46,40 +46,6 @@ wRdiMuonCandidateFilterPresel = cms.EDFilter("CandViewCountFilter",
 
 
 
-### make sure the evt has at least two jets, and one has a nontrivial pT
-muwRhardJet = cms.EDFilter("PATJetRefSelector",
-		src = cms.InputTag("slimmedJets"),
-		cut = cms.string("pt>20")
-		)
-
-muwRsoftJet = cms.EDFilter("PATJetRefSelector",
-		src = cms.InputTag("slimmedJets"),
-		cut = cms.string("pt>8")
-		)
-
-muwRdiJetCandidate = cms.EDProducer("CandViewShallowCloneCombiner",
-		decay = cms.string("muwRhardJet muwRsoftJet"),
-		role = cms.string("leadingJet subleadingJet"),
-		checkCharge = cms.bool(False),
-		cut = cms.string("mass > 0")
-		)
-
-muwRdiJetCandidateFilter = cms.EDFilter("CandViewCountFilter",
-		src = cms.InputTag("muwRdiJetCandidate"),
-		minNumber = cms.uint32(1)
-		)
-
-
-
-### select loose-ID jets
-#wRlooseJet = cms.EDFilter("PATJetSelector",
-#                            src = cms.InputTag("slimmedJets"),
-#                            cut = cms.string("(neutralHadronEnergyFraction<0.90 && neutralEmEnergyFraction<0.9 && (chargedMultiplicity+neutralMultiplicity)>1 && muonEnergyFraction<0.8) && ((abs(eta)<=2.4 && chargedHadronEnergyFraction>0 && chargedMultiplicity>0 && chargedEmEnergyFraction<0.90) || abs(eta)>2.4)"),
-#                            #cut = cms.string("pt>60"),
-#                            )
-
-
-
 ### create an object from two jets and two muons in the evt, and cut on its mass
 wRdiMuonDijetCandidate = cms.EDProducer("CandViewShallowCloneCombiner",
 		decay = cms.string("muwRdiJetCandidate wRdiMuonCandidate"),
@@ -98,43 +64,10 @@ wRdiMuonDijetCandidateFilter = cms.EDFilter("CandViewCountFilter",
 
 ### create di-muon pair in sideband region
 wRdiMuonSidebandCandidate = cms.EDProducer("CandViewShallowCloneCombiner",
-                                       decay = cms.string("wRleadingMuon wRsubleadingMuon"),
+                                       decay = cms.string("wRleadingMuonPresel wRsubleadingMuonPresel"),
                                        checkCharge = cms.bool(False),
                                        cut = cms.string("0< mass < 200 && daughter(0).pt>daughter(1).pt")
                                        )
 
-### filter: at least one di-muon candidate in sideband region
-wRdiMuonSidebandCandidateFilter = cms.EDFilter("CandViewCountFilter",
-                                                   src = cms.InputTag("wRdiMuonSidebandCandidate"),
-                                                   minNumber = cms.uint32(1)
-                                                   )
-
-### di-jet selection sequence
-muwRjetSelectionSeq = cms.Sequence(muwRhardJet + muwRsoftJet)
-### di-muon selection sequence
-wRmuonSelectionSeq = cms.Sequence(wRleadingMuon + wRsubleadingMuon)
-### di-muon selection in signal region sequence
-wRdiMuonSignalSeq = cms.Sequence(wRmuonSelectionSeq * wRdiMuonCandidate * wRdiMuonCandidateFilter)
-### di-muon and four object selection in signal region sequence
-wRdiMuonAndFourObjSignalSeq = cms.Sequence(
-		muwRjetSelectionSeq
-		*wRdiMuonSignalSeq
-		*muwRdiJetCandidate
-		*muwRdiJetCandidateFilter
-		*wRdiMuonDijetCandidate
-		*wRdiMuonDijetCandidateFilter
-		)
-### di-muon selection in sideband region sequence
-wRdiMuonSidebandSeq = cms.Sequence(wRmuonSelectionSeq * wRdiMuonSidebandCandidate * wRdiMuonSidebandCandidateFilter)
-
-### di-muon and low four object mass selection sequence
-wRdiMuonAndLowMassSignalSeq = cms.Sequence(
-		muwRjetSelectionSeq
-		*wRdiMuonSignalSeq
-		*muwRdiJetCandidate
-		*muwRdiJetCandidateFilter
-		*wRdiMuonDijetCandidate
-		*~wRdiMuonDijetCandidateFilter
-		)
 
 ### @}
