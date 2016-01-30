b97775f5f1dbeb57a0094f63932d8cc7654fd93f
Jorge Chaves <jec429@cornell.edu>
2016-01-30 19:57:08 +0100

    First version of the header file

--- interface/miniTreeEvent.h
+++ interface/miniTreeEvent.h
@@ -1,36 +1,100 @@
 class miniTreeEvent{
-    unsigned run;
-    unsigned lumi;
-    unsigned long long event;
+ public:
+  Int_t run;
+  Int_t lumi;
+  Long64_t event;
 
-    std::vector<TLorentzVector> electrons_p4;
-    std::vector<TLorentzVector> muons_p4;
-    std::vector<TLorentzVector> jets_p4;
+  // RunTime 
+  // UInt_t run_time;
+ 
+  std::vector<TLorentzVector> electrons_p4;
+  std::vector<TLorentzVector> muons_p4;
+  std::vector<TLorentzVector> jets_p4;
     
-    std::vector<float> jet_uncertainty;
-    std::vector<float> electron_scale;
-    std::vector<float> electron_smearing;
+  std::vector<float> jet_uncertainty;
+  std::vector<float> electron_scale;
+  std::vector<float> electron_smearing;
 
-    float PU;
-    float weight;
-        
-    miniTreeEvent(void) { clear(); };
+  Int_t nPU;
+  Int_t nPV;
+  Float_t weight;
 
-  };
+  miniTreeEvent(void){ clear(); };
+  void clear();
+  void SetBranches(TTree* tree);
+  void SetBranchAddresses(TTree* tree, miniTreeEvent& event);
+
+  std::vector<TLorentzVector> * electrons_p4_ptr;
+  std::vector<TLorentzVector> * muons_p4_ptr;
+  std::vector<TLorentzVector> * jets_p4_ptr;
+
+  std::vector<float> * jet_uncertainty_ptr;
+  std::vector<float> * electron_scale_ptr;
+  std::vector<float> * electron_smearing_ptr;
+
+};
 
 
-    void clear() {
-      run = lumi = event = 0;
+void miniTreeEvent::clear() {
+  run = lumi = event = 0;
     
-      electrons_p4.clear();
-      muons_p4.clear();
-      jets_p4.clear();
+  electrons_p4.clear();
+  muons_p4.clear();
+  jets_p4.clear();
 
-      jet_uncertainty.clear();
-      electron_scale.clear();
-      electron_smearing.clear();
+  jet_uncertainty.clear();
+  electron_scale.clear();
+  electron_smearing.clear();
 
-      PU = -999.;
-      weight = 0.0;
+  nPU = -999.;
+  nPV = 0.;
+  weight = 0.0;
       
-    }
+}
+
+void miniTreeEvent::SetBranches(TTree* tree) {
+  tree->Branch("run", &run);
+  tree->Branch("lumi", &lumi);
+  tree->Branch("event", &event);
+
+  tree->Branch("electrons_p4", &electrons_p4,32000,-1);
+  tree->Branch("muons_p4", &muons_p4,32000,-1);
+  tree->Branch("jets_p4", &jets_p4,32000,-1);
+
+  tree->Branch("jet_uncertainty",&jet_uncertainty);
+  tree->Branch("electron_scale",&electron_scale);
+  tree->Branch("electron_smearing",&electron_smearing);
+
+  tree->Branch("nPU", &nPU);
+  tree->Branch("nPV", &nPV);
+  tree->Branch("weight",&weight);
+
+}
+
+void miniTreeEvent::SetBranchAddresses(TTree* tree, miniTreeEvent& event) {
+  
+  event.electrons_p4_ptr = 0;
+  event.muons_p4_ptr = 0;
+  event.jets_p4_ptr = 0;
+
+  event.jet_uncertainty_ptr = 0;
+  event.electron_scale_ptr = 0;
+  event.electron_smearing_ptr = 0;
+
+  tree->SetBranchAddress("run",&event.run);
+  tree->SetBranchAddress("lumi", &event.lumi);
+  tree->SetBranchAddress("event", &event.event);
+
+  tree->SetBranchAddress("electrons_p4", &event.electrons_p4_ptr);
+  tree->SetBranchAddress("muons_p4", &event.muons_p4_ptr);
+  tree->SetBranchAddress("jets_p4", &event.jets_p4_ptr);
+
+  tree->SetBranchAddress("jet_uncertainty",&event.jet_uncertainty_ptr);
+  tree->SetBranchAddress("electron_scale",&event.electron_scale_ptr);
+  tree->SetBranchAddress("electron_smearing",&event.electron_smearing_ptr);
+
+  tree->SetBranchAddress("nPU", &event.nPU);
+  tree->SetBranchAddress("nPV", &event.nPV);
+  tree->SetBranchAddress("weight",&event.weight);
+
+}
--- plugins/miniTTree.cc
+++ plugins/miniTTree.cc
@@ -11,6 +11,8 @@
 #include "DataFormats/JetReco/interface/Jet.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 #include "DataFormats/Common/interface/ValueMap.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
+#include "../interface/miniTreeEvent.h"
 
 typedef double JECUnc_t;
 typedef edm::ValueMap<JECUnc_t> JECUnc_Map;
@@ -25,12 +27,15 @@ private:
   edm::EDGetToken electronsMiniAODToken_;
   edm::EDGetToken muonsMiniAODToken_;
   edm::EDGetToken jetsMiniAODToken_;
+  edm::EDGetToken pileUpInfoToken_;
+  edm::EDGetToken primaryVertexToken_;
+  edm::EDGetToken evinfoToken_;
 
   const std::string jec_unc_src;
 
-
   TTree* tree;
-  tree_t nt;
+  miniTreeEvent myEvent;
+
 };
 
 miniTTree::miniTTree(const edm::ParameterSet& cfg)
@@ -39,82 +44,81 @@ miniTTree::miniTTree(const edm::ParameterSet& cfg)
   electronsMiniAODToken_   = mayConsume<edm::View<pat::Electron> >(cfg.getParameter<edm::InputTag>("electrons_src"));
   muonsMiniAODToken_ = mayConsume<edm::View<pat::Muon> >(cfg.getParameter<edm::InputTag>("muons_src"));
   jetsMiniAODToken_ = mayConsume<edm::View<pat::Jet> >(cfg.getParameter<edm::InputTag>("jets_src"));
+  pileUpInfoToken_ = mayConsume<edm::View<PileupSummaryInfo> >(edm::InputTag("slimmedAddPileupInfo"));
+  primaryVertexToken_ = mayConsume<edm::View<reco::Vertex> >(edm::InputTag("offlineSlimmedPrimaryVertices"));
+  evinfoToken_ = mayConsume<GenEventInfoProduct>(edm::InputTag("generator"));
 
   edm::Service<TFileService> fs;
   tree = fs->make<TTree>("t", "");
 
-  miniTreeEvent myEvent;
   myEvent.SetBranches(tree);
-  myEvent.SetBranchAddresses(tree);
-
-
-  tree->Branch("run", &nt.run);
-  tree->Branch("lumi", &nt.lumi);
-  tree->Branch("event", &nt.event);
-
-  tree->Branch("electrons_p4", &nt.electrons_p4,32000,-1);
-  tree->Branch("muons_p4", &nt.muons_p4,32000,-1);
-  tree->Branch("jets_p4", &nt.jets_p4,32000,-1);
-
-  tree->Branch("jet_uncertainty",&nt.jet_uncertainty);
-  tree->Branch("electron_scale",&nt.electron_scale);
-  tree->Branch("electron_smearing",&nt.electron_smearing);
-
-  tree->Branch("PU", &nt.PU, "PU/F");
-  tree->Branch("weight",&nt.weight,"weight/F");
+  //myEvent.SetBranchAddresses(tree);
 
 }
 
 void miniTTree::analyze(const edm::Event& event, const edm::EventSetup&) {
-  nt.clear();
-  nt.run = event.id().run();
-  nt.lumi = event.luminosityBlock();
-  nt.event = event.id().event();
-
+  myEvent.clear();
+  myEvent.run = event.id().run();
+  myEvent.lumi = event.luminosityBlock();
+  myEvent.event = event.id().event();
+  
   edm::Handle<edm::View<pat::Electron> > electrons;
   event.getByToken(electronsMiniAODToken_,electrons);
   edm::Handle<edm::View<pat::Muon> > muons;
   event.getByToken(muonsMiniAODToken_,muons);
   edm::Handle<edm::View<pat::Jet> > jets;
   event.getByToken(jetsMiniAODToken_,jets);
-  edm::Handle<GenEventInfoProduct> evinfo;
 
   edm::Handle< edm::ValueMap<double> > jec_unc;
   event.getByLabel(jec_unc_src, "JECUncertainty", jec_unc);
 
+  edm::Handle<GenEventInfoProduct> evinfo;
+  edm::Handle<edm::View<PileupSummaryInfo> > PU_Info;
+
+  edm::Handle<edm::View<reco::Vertex> > primary_vertex;
+  event.getByToken(primaryVertexToken_,primary_vertex);
+
+  if(primary_vertex->size() > 0) {
+    for(auto pv: *primary_vertex)
+      myEvent.nPV++;
+  }
+  
   if(!event.isRealData()) 
     {
-      event.getByLabel("generator", evinfo);
-      nt.weight = evinfo->weight();
-    }
-
-  nt.PU = 1.0;
+      event.getByToken(evinfoToken_, evinfo);
+      myEvent.weight = evinfo->weight();
+      event.getByToken(pileUpInfoToken_, PU_Info);
+      for(auto p: *PU_Info){
+	int BX = p.getBunchCrossing();
+	if(BX==0)
+	  myEvent.nPU = p.getTrueNumInteractions();
+      }
+    }  
   
   for (size_t i = 0; i < electrons->size(); ++i){
     const auto ele = electrons->ptrAt(i);
     TLorentzVector p4;
     p4.SetPtEtaPhiM(ele->pt(),ele->eta(),ele->phi(),ele->mass());
-    nt.electrons_p4.push_back(p4);
-    nt.electron_scale.push_back(1.0);
-    nt.electron_smearing.push_back(1.0);
+    myEvent.electrons_p4.push_back(p4);
+    myEvent.electron_scale.push_back(1.0);
+    myEvent.electron_smearing.push_back(1.0);
   }
+
   for (size_t i = 0; i < muons->size(); ++i){
     const auto mu = muons->ptrAt(i);
     TLorentzVector p4;
     p4.SetPtEtaPhiM(mu->pt(),mu->eta(),mu->phi(),mu->mass());
-    nt.muons_p4.push_back(p4);
+    myEvent.muons_p4.push_back(p4);
   }
+
   for (size_t i = 0; i < jets->size(); ++i){
     const auto jet = jets->ptrAt(i);
     TLorentzVector p4;
     p4.SetPtEtaPhiM(jet->pt(),jet->eta(),jet->phi(),jet->mass());
-    nt.jets_p4.push_back(p4);
-    nt.jet_uncertainty.push_back((*jec_unc)[jet]);
+    myEvent.jets_p4.push_back(p4);
+    myEvent.jet_uncertainty.push_back((*jec_unc)[jet]);
   }
 
-
-
-  
   tree->Fill();
 }
 
