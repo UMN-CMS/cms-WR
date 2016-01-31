a258b65493e6d82f879b3e2765ffcee283b163d3
Shervin <shervin@cern.ch>
2016-01-31 23:32:44 +0100

    pileup weights in the sequence now

--- python/pileupWeight_cfi.py
+++ python/pileupWeight_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # Pileup root files should contain a TH1F in its rootdir named "pileup"
-PUWeights = cms.EDProducer("PileupWeight",
+PUWeights = cms.EDProducer("producePileupWeight",
 		outputCollectionName = cms.string("PileupWeights"),
 		PileupMCFilename = cms.string("data/MCPileup.root"),
 		PileupDataFilename = cms.string("data/DataPileup.root"),
--- test/runAnalysis_cfg.py
+++ test/runAnalysis_cfg.py
@@ -124,8 +124,8 @@ process.load('ExoAnalysis.cmsWR.minitree_cfi')
 process.blindSeq = cms.Sequence()
 #process.dumperSeq = cms.Sequence(process.MakeTTree_Muons)
 process.miniTTreeSeq = cms.Sequence(process.JECUnc * process.MiniTTree)
-#process.fullSeq = cms.Sequence(process.addStringIdentifier * process.PUWeightsSequence * process.jecSequence * process.selectionSequence * process.filterSequence)
-process.fullSeq = cms.Sequence(process.addStringIdentifier *  process.jecSequence * process.selectionSequence * process.filterSequence)
+process.fullSeq = cms.Sequence(process.addStringIdentifier * process.PUWeightsSequence * process.jecSequence * process.selectionSequence * process.filterSequence)
+#process.fullSeq = cms.Sequence(process.addStringIdentifier *  process.jecSequence * process.selectionSequence * process.filterSequence)
 
 
 ############################################################ PATHs definition
