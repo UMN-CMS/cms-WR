521651349aafacb47b597a4ce5cc958c68d60cf7
Shervin <shervin@cern.ch>
2016-01-31 16:43:31 +0100

    mayconsumes to be changed to consumes, getByLabel to getByToken, errors to be included in miniTree

--- python/minitree_cfi.py
+++ python/minitree_cfi.py
@@ -4,7 +4,10 @@ MiniTTree = cms.EDAnalyzer('miniTTree',
                             muons_src = cms.InputTag('wRsubleadingMuon'),
                             electrons_src = cms.InputTag('wRminiTreeElectron'),
                             jets_src = cms.InputTag('wRJets'),
-                            jec_unc_src = cms.string('wRJECUncert'),
-                            muon_IDSF_central_src = cms.string(''),
-                            muon_IsoSF_central_src = cms.string(''),
+                            jec_unc_src = cms.InputTag('wRJECUncert'),
+                            muon_IDSF_central_src = cms.InputTag('muonIdIsoSF','MuonSFIdCentral'),
+                           muon_IsoSF_central_src = cms.InputTag('muonIdIsoSF', 'MuonSFIsoCentral'),
+#                OutputCollectionName2 = cms.string("MuonSFIdError"),
+#                OutputCollectionName4 = cms.string("MuonSFIsoError"),
+
                             )
