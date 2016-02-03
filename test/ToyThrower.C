f244fedd4777f04d761a7b696546eedbc15476f4
Rajdeep <r.m.chatterjeejr@gmail.com>
2016-02-03 13:45:46 +0100

    ToyThrower modified to read a vector of strings to decide which of the systematics are to have toys thrown on, and the Random number pointer is taken as input by the function from the tmp.C main skeleton. Currently the list of systematics is read from a txt file Systematics_To_Be_Evaluated.txt

--- test/ToyThrower.C
+++ test/ToyThrower.C
@@ -11,51 +11,80 @@
 #include "TFile.h"
 #include "../plugins/rochcor2015.h"
 #include "../plugins/muresolution_run2.h"
-using namespace std;
+#include "../plugins/rochcor2015.C"
+#include "../plugins/muresolution_run2.C"
 
-void ToyThrower(miniTreeEvent myEvent, int RandomSeed){
-      int DEBUG = 0;
+void ToyThrower(miniTreeEvent myEvent, TRandom3* rand, int random_seed, std::vector<string> list, int isData){
+
+      int DEBUG = 1;
       float qter = 1.0;
-      TRandom3* Rand=new TRandom3(); 
-      Rand->SetSeed(RandomSeed);
       double Smear = 0.0;
       int Iterator = 0;
-//      rochcor2015 *rmcor = new rochcor2015(RandomSeed);
+      int Flag_Smear_Muon_Scale=0,Flag_Smear_Muon_ID_Iso=0,Flag_Smear_Electron_Scale=0,Flag_Smear_Jet_Scale=0;
+
+      for(unsigned int iii=0;iii<list.size();iii++){
+              if(list[iii] == "Smear_Muon_Scale")     Flag_Smear_Muon_Scale=1;
+         else if(list[iii] == "Smear_Muon_ID_Iso")    Flag_Smear_Muon_ID_Iso=1;
+         else if(list[iii] == "Smear_Electron_Scale") Flag_Smear_Electron_Scale=1;
+         else if(list[iii] == "Smear_Jet_Scale")      Flag_Smear_Jet_Scale=1;
+        }
+
+      rochcor2015 *rmcor = new rochcor2015(random_seed);
+
       for(auto muons:*(myEvent.muons_p4)){
-          Smear=Rand->Gaus(0.0,1);
           if(DEBUG) cout<<endl<<" Muon number= "<<Iterator<<" Muon Pt Before = "<<(*(myEvent.muons_p4))[Iterator].Pt()<<" Muon Eta Before = "<<(*(myEvent.muons_p4))[Iterator].Eta()<<endl;
-          if(Smear >= 0.) (*(myEvent.muon_IDSF_central))[Iterator] += Smear*(*(myEvent.muon_IDSF_error))[Iterator];  
-          else            (*(myEvent.muon_IDSF_central))[Iterator] -= Smear*(*(myEvent.muon_IDSF_error))[Iterator];
 
-          Smear=Rand->Gaus(0.0,1);
-          if(Smear >= 0.) (*(myEvent.muon_IsoSF_central))[Iterator] += Smear*(*(myEvent.muon_IsoSF_error))[Iterator];                            
-          else            (*(myEvent.muon_IsoSF_central))[Iterator] -= Smear*(*(myEvent.muon_IsoSF_error))[Iterator];
+          if(Flag_Smear_Muon_ID_Iso && !isData ){
+             Smear=rand->Gaus(0.0,1);
+             if(Smear >= 0.) (*(myEvent.muon_IDSF_central))[Iterator] += Smear*(*(myEvent.muon_IDSF_error))[Iterator];  
+             else            (*(myEvent.muon_IDSF_central))[Iterator] -= Smear*(*(myEvent.muon_IDSF_error))[Iterator];
 
-//          rmcor->momcor_mc((*(myEvent.muons_p4))[Iterator], (*(myEvent.muon_charge))[Iterator], 0, qter);
+             Smear=rand->Gaus(0.0,1);
+             if(Smear >= 0.) (*(myEvent.muon_IsoSF_central))[Iterator] += Smear*(*(myEvent.muon_IsoSF_error))[Iterator];                            
+             else            (*(myEvent.muon_IsoSF_central))[Iterator] -= Smear*(*(myEvent.muon_IsoSF_error))[Iterator];
+            }
+
+          if(Flag_Smear_Muon_Scale){
+             if(isData) 
+                  rmcor->momcor_data((*(myEvent.muons_p4))[Iterator], (*(myEvent.muon_charge))[Iterator], 0, qter);
+//             else rmcor->momcor_mc((*(myEvent.muons_p4))[Iterator], (*(myEvent.muon_charge))[Iterator], 0, qter);
+            }
+
           if(DEBUG) cout<<endl<<" Muon number= "<<Iterator<<" Muon Pt After = "<<(*(myEvent.muons_p4))[Iterator].Pt()<<" Muon Eta After = "<<(*(myEvent.muons_p4))[Iterator].Eta()<<endl;
+
           Iterator++;
         }
 
       Iterator = 0;
 
       for(auto electrons:*(myEvent.electrons_p4)){
-          Smear=Rand->Gaus(0.0,1);
+          Smear=rand->Gaus(0.0,1);
           if(DEBUG) cout<<endl<<" Electron number= "<<Iterator<<" Electron Pt Before = "<<(*(myEvent.electrons_p4))[Iterator].Pt()<<" Electron Eta Before = "<<(*(myEvent.electrons_p4))[Iterator].Eta()<<endl;
-          (*(myEvent.electrons_p4))[Iterator] = (1 + (Smear)*(*(myEvent.electron_smearing))[Iterator])*(*(myEvent.electrons_p4))[Iterator]; 
+
+          if(Flag_Smear_Muon_Scale){
+            if(isData)
+                           (*(myEvent.electrons_p4))[Iterator] = (1 + (Smear)*(*(myEvent.electron_scale))[Iterator])*(*(myEvent.electrons_p4))[Iterator];
+            else           (*(myEvent.electrons_p4))[Iterator] = (1 + (Smear)*(*(myEvent.electron_smearing))[Iterator])*(*(myEvent.electrons_p4))[Iterator];
+            }
+
           if(DEBUG) cout<<endl<<" Electron number= "<<Iterator<<" Electron Pt After = "<<(*(myEvent.electrons_p4))[Iterator].Pt()<<" Electron Eta After = "<<(*(myEvent.electrons_p4))[Iterator].Eta()<<endl;
+
           Iterator++;  
         }
 
       Iterator = 0;
 
       for(auto jets:*(myEvent.jets_p4)){
-          Smear=Rand->Gaus(0.0,1);
+          Smear=rand->Gaus(0.0,1);
           if(DEBUG) cout<<endl<<" Jet number= "<<Iterator<<" Jet Pt Before = "<<(*(myEvent.jets_p4))[Iterator].Pt()<<" Jet Eta Before = "<<(*(myEvent.jets_p4))[Iterator].Eta()<<endl;
-          (*(myEvent.jets_p4))[Iterator] = (1 + (Smear)*(*(myEvent.jec_uncertainty))[Iterator])*(*(myEvent.jets_p4))[Iterator];
+
+          if(Flag_Smear_Jet_Scale)
+            (*(myEvent.jets_p4))[Iterator] = (1 + (Smear)*(*(myEvent.jec_uncertainty))[Iterator])*(*(myEvent.jets_p4))[Iterator];
+
           if(DEBUG) cout<<endl<<" Jet number= "<<Iterator<<" Jet Pt Before = "<<(*(myEvent.jets_p4))[Iterator].Pt()<<" Jet Eta After = "<<(*(myEvent.jets_p4))[Iterator].Eta()<<endl;
+
           Iterator++;
         }
 
-//      delete rmcor;
 }
 
--- test/tmp.C
+++ test/tmp.C
@@ -5,6 +5,8 @@
 #include "RooDataSet.h"
 #include "TRandom3.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "../interface/miniTreeEvent.h"
 #include "../interface/FitRooDataSet.h"
 #include "rooFitFxns.C"
@@ -17,7 +19,7 @@
 void tmp(){
 
   using namespace RooFit;
-  
+ 
   TFile * hfile = new TFile("/afs/cern.ch/user/j/jchavesb/public/WR/ttree.root");
   TTree *tree = (TTree*)hfile->Get("MiniTTree/t");  
   
@@ -26,9 +28,19 @@ void tmp(){
   myEvent.SetBranchAddresses(tree, myEvent);
   
   int nToys = 1;
+  int isData= 1;// Fill in with 1 or 0 based on information from the trees
+  TRandom3* Rand=new TRandom3();
+  vector<string> List_Systematics;
+  string word;
+  ifstream Syst_File;
+  Syst_File.open("Systematics_To_Be_Evaluated.txt");
+  while(!Syst_File.eof()){
+       Syst_File>>word;      
+       List_Systematics.push_back(word);
+      }
 
   for(int i = 0; i < nToys; i++){
-
+    Rand->SetSeed(i+1);
     RooRealVar massWR("fourObjectMass", "fourObjectMass", 600,6500);
     RooRealVar evtWeight("evtWeight", "evtWeight", -2,2);
     RooArgSet vars(massWR,evtWeight);
@@ -37,7 +49,7 @@ void tmp(){
     for(int ev = 0; ev<tree->GetEntries(); ev++){
       tree->GetEntry(ev);
       
-      ToyThrower(myEvent, ev+1); 
+      ToyThrower(myEvent, Rand, i+1, List_Systematics, isData); 
 
       massWR.setVal(1.0);
       evtWeight.setVal(1.0);
@@ -55,8 +67,7 @@ void tmp(){
 
     std::cout<<"Res="<<std::endl;
     expPdfRooAbsPdf->Print();
-
   }
 
-
+  delete Rand;
 }
