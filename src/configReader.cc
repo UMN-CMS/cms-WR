05377bd82a38af9141783d6d5486e343706e5a6b
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-03-16 10:54:55 +0100

    Fixed config file

--- src/configReader.cc
+++ src/configReader.cc
@@ -55,7 +55,7 @@ configReader::configReader(std::string filename)
 TChain *configReader::getMiniTreeChain(std::string datasetName, std::string tag)
 {
 	TChain *chain = new TChain((tag + "/t").c_str(), "");
-	chain->Add(("root://xrootd-cms.infn.it//store/user/shervin/ntuples/" + datasetName + configFile["productionTAG"] + "/unmerged-allRange.root").c_str());
+	chain->Add(("root://eoscms.cern.ch//store/user/shervin/ntuples/" + datasetName + configFile["productionTAG"] + "/unmerged-allRange.root").c_str());
 	chain->GetEntries();
 	return chain;
 }
@@ -65,6 +65,7 @@ TChain *configReader::getMiniTreeChain(std::vector<std::string> datasetNames, st
 
 	TChain *chain = new TChain((tag + "/t").c_str(), "");
 	for(auto s : datasetNames) {
+		std::cout << "[INFO] Reading chain for: " << s << "\t" << tag << std::endl;
 		TChain *c = getMiniTreeChain(s, tag);
 		chain->Add(c);
 		delete c;
@@ -99,6 +100,11 @@ void configReader::datasetsFileReader(std::string filename)
 
 		f_in >> datasetName >> line;
 		configMap[datasetName] = line;
+#ifdef DEBUG
+		std::cout << "##"<<datasetName <<"##"<< "\t" << configMap.size() << std::endl;
+		std::cout << configMap.begin()->first << "\t" << configMap.count(datasetName) << std::endl;
+		//assert(checkCategory(datasetName)==false);
+#endif
 	}
 
 
