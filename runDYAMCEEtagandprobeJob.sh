abefe82e06f37b35ff274cf336a3b4377e152ee6
skalafut <kalaf004@umn.edu>
2016-06-03 15:26:34 +0200

    adding lxplus batch submission scripts used to process dytagandprobe minitrees for DY MC and real data in EE channel

---
+++ runDYAMCEEtagandprobeJob.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#use this file with a command like
+#bsub -R "rusage[mem=13000]" -q 8nh -J DYAMCEEtagandprobe < runDYAMCEEtagandprobeJob.sh
+#to request 13 GB ram
+CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
+UP=/afs/cern.ch/user/s/skalafut/WR
+#finalDir=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/
+finalDir=/afs/cern.ch/user/s/skalafut/WR/CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
+export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
+
+cd $UP/$CMSSW_PROJECT_SRC
+eval `scramv1 runtime -sh`
+eval 'make'
+eval "./bin/analysis -m DYAMC -c EE --isTagAndProbe true"
+#eval "mv ./selected_tree_DYAMC_dytagandprobeEE.root $finalDir/."
+
