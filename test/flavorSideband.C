402f610600b007fa73f642b598195f32cf8383ab
skalafut <kalaf004@umn.edu>
2016-06-23 21:34:30 +0200

    updated flavorSideband.C to produce plots with rescaled emu data and mumu or ee MC

--- test/flavorSideband.C
+++ test/flavorSideband.C
@@ -1,3 +1,4 @@
+#include "TStyle.h"
 #include "TH1F.h"
 #include "TF1.h"
 #include "TH2F.h"
@@ -22,28 +23,38 @@
 void fillHisto(TChain * chain, Selector *myEvent, TH1F * h);
 void flavorSideband(){
 
+  gStyle->SetOptFit(1111);
   TChain * chain_EMu = new TChain("Tree_Iter0");
   TChain * chain_EE = new TChain("Tree_Iter0");
   TChain * chain_MuMu = new TChain("Tree_Iter0");
+  TChain * chain_EMuData = new TChain("Tree_Iter0");
  
   TString dir = "../";
   chain_EMu->Add(dir+"selected_tree_TT_flavoursidebandEMu.root");
   chain_EE->Add(dir+"selected_tree_TT_signal_eeEE.root");
   chain_MuMu->Add(dir+"selected_tree_TT_signal_mumuMuMu.root");
-
+  chain_EMuData->Add(dir+"selected_tree_data_flavoursidebandEMu.root");
+ 
   Selector myEvent_EMu;
+  Selector myEvent_EMuData;
   Selector myEvent_EE;
   Selector myEvent_MuMu;
 
   myEvent_EMu.SetBranchAddresses(chain_EMu);
+  myEvent_EMuData.SetBranchAddresses(chain_EMuData);
   myEvent_EE.SetBranchAddresses(chain_EE);
   myEvent_MuMu.SetBranchAddresses(chain_MuMu);
 
+  //TH1F *h_WR_mass_EMu = new TH1F("h_WR_mass_EMu","",50,200,4000);
+  //TH1F *h_WR_mass_EE = new TH1F("h_WR_mass_EE","",50,200,4000);
+  //TH1F *h_WR_mass_MuMu = new TH1F("h_WR_mass_MuMu","",50,200,4000);
   TH1F *h_WR_mass_EMu = new TH1F("h_WR_mass_EMu","",50,200,2000);
   TH1F *h_WR_mass_EE = new TH1F("h_WR_mass_EE","",50,200,2000);
   TH1F *h_WR_mass_MuMu = new TH1F("h_WR_mass_MuMu","",50,200,2000);
+  TH1F *h_WR_mass_EMuData = new TH1F("h_WR_mass_EMuData","",50,200,2000);
   
   fillHisto(chain_EMu, &myEvent_EMu, h_WR_mass_EMu);
+  fillHisto(chain_EMuData, &myEvent_EMuData, h_WR_mass_EMuData);
   fillHisto(chain_EE, &myEvent_EE, h_WR_mass_EE);
   fillHisto(chain_MuMu, &myEvent_MuMu, h_WR_mass_MuMu);
 
@@ -58,7 +69,6 @@ void flavorSideband(){
   mycanvas_EE->Print(("flavor_EE.pdf"));
   mycanvas_EE->Print(("flavor_EE.png"));
 
-
   TCanvas* mycanvas_MuMu = new TCanvas( "mycanvas_MuMu", "", 0, 0, 600, 600 ) ;
   h_WR_mass_EMu->DrawNormalized();
   h_WR_mass_MuMu->SetLineColor(kRed);
@@ -70,13 +80,13 @@ void flavorSideband(){
   mycanvas_MuMu->Print(("flavor_MuMu.pdf"));
   mycanvas_MuMu->Print(("flavor_MuMu.png"));
 
-
   TH1F *h_ratio_EE = (TH1F*)h_WR_mass_EE->Clone();
   TH1F *h_ratio_MuMu = (TH1F*)h_WR_mass_MuMu->Clone();
   h_ratio_EE->Divide(h_WR_mass_EMu);
   h_ratio_MuMu->Divide(h_WR_mass_EMu);
   TCanvas* mycanvas_ratio_EE = new TCanvas( "mycanvas_ratio_EE", "", 0, 0, 600, 600 ) ;
-  TF1 *f_EE = new TF1("f_EE","[0]*x+[1]",600,2000);
+  TF1 *f_EE = new TF1("f_EE","[0]",600,2000);
+  f_EE->FixParameter(0,0.427);
   h_ratio_EE->Fit("f_EE");
   h_ratio_EE->Draw();
   f_EE->SetLineColor(kBlue);
@@ -86,7 +96,8 @@ void flavorSideband(){
 
 
   TCanvas* mycanvas_ratio_MuMu = new TCanvas( "mycanvas_ratio_MuMu", "", 0, 0, 600, 600 ) ;
-  TF1 *f_MuMu = new TF1("f_MuMu","[0]*x+[1]",600,2000);
+  TF1 *f_MuMu = new TF1("f_MuMu","[0]",600,2000);
+  f_MuMu->FixParameter(0,0.64);
   h_ratio_MuMu->Fit("f_MuMu");
   h_ratio_MuMu->Draw();
   f_MuMu->SetLineColor(kBlue);
@@ -94,6 +105,57 @@ void flavorSideband(){
   mycanvas_ratio_MuMu->Print(("flavor_ratio_MuMu.pdf"));
   mycanvas_ratio_MuMu->Print(("flavor_ratio_MuMu.png"));
 
+  TCanvas* canvMuMuEMu = new TCanvas("canvMuMuEMu","",600,600);
+  canvMuMuEMu->cd();
+  TLegend * legMuMuEMu = new TLegend(0.72,0.6,0.98,0.8);
+  legMuMuEMu->AddEntry(h_WR_mass_EMu,"EMu");
+  legMuMuEMu->AddEntry(h_WR_mass_MuMu,"MuMu");
+  h_WR_mass_EMu->Draw("histo");
+  h_WR_mass_MuMu->Draw("Psame");
+  legMuMuEMu->Draw();
+  canvMuMuEMu->SaveAs("emujj_and_mumujj_signal_region.pdf","recreate");
+  canvMuMuEMu->SaveAs("emujj_and_mumujj_signal_region.png","recreate");
+
+  TCanvas* canvMuMuEMuData = new TCanvas("canvMuMuEMuData","",600,600);
+  canvMuMuEMuData->cd();
+  TLegend * legMuMuEMuData = new TLegend(0.72,0.6,0.98,0.8);
+  legMuMuEMuData->AddEntry(h_WR_mass_EMuData,"Rescaled EMu Data");
+  legMuMuEMuData->AddEntry(h_WR_mass_MuMu,"MuMu MC");
+  h_WR_mass_MuMu->Draw("histo");
+  h_WR_mass_EMuData->Scale(0.64);
+  h_WR_mass_EMuData->SetMarkerStyle(2);
+  h_WR_mass_EMuData->SetMarkerSize(2);
+  h_WR_mass_EMuData->Draw("Psame");
+  legMuMuEMuData->Draw();
+  canvMuMuEMuData->SaveAs("rescaled_emujj_data_and_mumujj_MC_signal_region.pdf","recreate");
+  canvMuMuEMuData->SaveAs("rescaled_emujj_data_and_mumujj_MC_signal_region.png","recreate");
+
+
+  TCanvas* canvEEEMu = new TCanvas("canvEEEMu","",600,600);
+  canvEEEMu->cd();
+  TLegend * legEEEMu = new TLegend(0.72,0.6,0.98,0.8);
+  legEEEMu->AddEntry(h_WR_mass_EMu,"EMu");
+  legEEEMu->AddEntry(h_WR_mass_EE,"EE");
+  h_WR_mass_EMu->Draw("histo");
+  h_WR_mass_EE->Draw("Psame");
+  legEEEMu->Draw();
+  canvEEEMu->SaveAs("emujj_and_eejj_signal_region.pdf","recreate");
+  canvEEEMu->SaveAs("emujj_and_eejj_signal_region.png","recreate");
+
+  TCanvas* canvEEEMuData = new TCanvas("canvEEEMuData","",600,600);
+  canvEEEMuData->cd();
+  TLegend * legEEEMuData = new TLegend(0.72,0.6,0.98,0.8);
+  legEEEMuData->AddEntry(h_WR_mass_EMuData,"Rescaled EMu Data");
+  legEEEMuData->AddEntry(h_WR_mass_EE,"EE MC");
+  h_WR_mass_EE->Draw("histo");
+  h_WR_mass_EMuData->Scale(1/0.64);	///<undo the scaling which was done earlier
+  h_WR_mass_EMuData->SetMarkerStyle(2);
+  h_WR_mass_EMuData->SetMarkerSize(2);
+  h_WR_mass_EMuData->Scale(0.427);
+  h_WR_mass_EMuData->Draw("Psame");
+  legEEEMuData->Draw();
+  canvEEEMuData->SaveAs("rescaled_emujj_data_and_eejj_MC_signal_region.pdf","recreate");
+  canvEEEMuData->SaveAs("rescaled_emujj_data_and_eejj_MC_signal_region.png","recreate");
 
   TFile f("flavor_fits.root","RECREATE");
   h_ratio_EE->Write();
@@ -114,4 +176,5 @@ void fillHisto(TChain * chain, Selector *myEvent, TH1F * h){
     if(myEvent->WR_mass > 600. && myEvent->dilepton_mass > 200.) 
       h->Fill(myEvent->WR_mass,myEvent->weight);
   }
+  //std::cout<<"histo named\t"<< h->GetName() <<"\thas integral\t"<< h->Integral() <<std::endl;
 }
