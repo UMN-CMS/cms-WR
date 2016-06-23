65a4ac72167b9d6e302be10f3c5365efd0a06e10
Peter <phansen@physics.umn.edu>
2016-06-23 19:59:19 +0200

    added missing SelectorHist.h

---
+++ interface/SelectorHist.h
@@ -0,0 +1,35 @@
+
+#ifndef selectorhist_h
+#define selectorhist_h
+
+#include "TH1F.h"
+#include <map>
+#include <memory>
+#include <string>
+
+typedef std::shared_ptr<TH1F> hist_p;
+class SelectorHist
+{
+public:
+	SelectorHist() {}
+	~SelectorHist()
+	{
+		histograms.clear();
+	}
+
+	hist_p operator()(std::string name, int nbins, float low, float hi);
+	void PrintEntries(std::string folder, std::string tag);
+	void Draw(std::string folder, std::string tag);
+	void Clear();
+
+private:
+	std::map<std::string, hist_p > histograms;
+	std::vector<std::string> histogram_names;
+
+};
+
+namespace sel {
+	extern SelectorHist hists;
+}
+
+#endif
