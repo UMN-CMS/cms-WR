52dd2d89f1118510b54f36f9bf5e6c05f9ec8aae
skalafut <kalaf004@umn.edu>
2016-06-26 17:05:44 +0200

    modified run EEtagandprobe bash scripts in cmsWR dir

--- runDataEEtagandprobeJob.sh
+++ runDataEEtagandprobeJob.sh
@@ -3,14 +3,11 @@
 #bsub -R "rusage[mem=6000]" -q 1nh -J DataEEtagandprobe < runDataEEtagandprobeJob.sh
 #to request 6 GB ram
 CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
-UP=/afs/cern.ch/user/s/skalafut/WR
-#finalDir=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/
-finalDir=/afs/cern.ch/user/s/skalafut/WR/CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
+UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment
 export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
 
 cd $UP/$CMSSW_PROJECT_SRC
 eval `scramv1 runtime -sh`
 eval 'make'
-eval "./bin/analysis -m data -c EE --isTagAndProbe true -f withEleScaleCorrOn"
-#eval "mv ./selected_tree_data_dytagandprobeEE.root $finalDir/."
+eval "./bin/analysis -m data -c EE --isTagAndProbe true --ignoreDyScaleFactors false"
 
