a467769c87493d696fbb985bf55b80d0a590bcc4
Peter <phansen@physics.umn.edu>
2016-07-11 05:57:54 +0200

    updated plotscript for datacards/summary.txt

--- scripts/sig_bg_plot.py
+++ scripts/sig_bg_plot.py
@@ -17,7 +17,7 @@ mass = np.ndarray(25)
 ele_results = np.ndarray((3,25))
 mu_results = np.ndarray((3,25))
 linei=0
-with open("sig_bg.txt","r") as f:
+with open("datacards/sig_bg.txt","r") as f:
 	for line in f:
 		if line[0] == '#': continue
 		line = map(float,line.split())
@@ -28,20 +28,29 @@ with open("sig_bg.txt","r") as f:
 		linei += 1
 
 h = ROOT.TH1F("h","",1,700, 6200)
-
+h.GetYaxis().SetTitleOffset(1.3)
 h.SetMinimum(.0001)
-h.SetMaximum(ele_results.max()*2)
-h.GetYaxis().SetTitleOffset(1.2)
+
 
 #electrons
 c = ROOT.TCanvas("c","c",800,800)
-c.SetLeftMargin(1.5)
+c.SetLeftMargin(.15)
 c.SetLogy()
 h.Draw()
 h.SetXTitle("W_R Mass [GeV]")
 h.SetYTitle("Events")
+h.SetMaximum(ele_results.max()*2)
 
-leg = ROOT.TLegend(.5, .7, .85, .9)
+inset = ROOT.TPad("inset","",.17, .15, .55, .55)
+inset.Draw()
+inset.cd()
+ins = ROOT.TH1F("ins","",1,700, 6200)
+ins.SetMaximum(10)
+ins.Draw()
+
+c.cd()
+
+leg = ROOT.TLegend(.5, .7, .8, .9)
 #leg.SetNColumns(3)
 leg.SetTextFont(42)
 leg.SetTextSize(0.032)
@@ -49,51 +58,60 @@ leg.SetBorderSize(0)
 
 graphs = []
 for n,col in enumerate(ele_results):
-	print ele_names[n]
 	graphs.append( ROOT.TGraph(len(mass), mass, col))
 	g = graphs[-1]
 	g.SetLineColor(colors[n])
 	g.SetLineWidth(3)
+	c.cd()
 	g.Draw("Lsame")
 	leg.AddEntry(g, ele_names[n], "L")
+	inset.cd()
+	g.Draw("Lsame")
 
+c.cd()
 leg.Draw("same")
 c.SaveAs("plots/ele_events.png")
 c.SaveAs("plots/ele_events.pdf")
 c.SaveAs("plots/ele_events.C")
-c.SetLogy(0)
-h.SetMaximum(ele_results.max()*1.1)
-h.SetMaximum(10)
-c.SaveAs("plots/ele_events_lin.png")
-c.SaveAs("plots/ele_events_lin.pdf")
-c.SaveAs("plots/ele_events_lin.C")
 
-
-h.SetMaximum(mu_results.max()*2)
+#muons
+c = ROOT.TCanvas("c","c",800,800)
+c.SetLeftMargin(.15)
+c.SetLogy()
 h.Draw()
+h.SetXTitle("W_R Mass [GeV]")
+h.SetYTitle("Events")
+h.SetMaximum(mu_results.max()*2)
 
-leg = ROOT.TLegend(.5, .7, .85, .9)
+inset = ROOT.TPad("inset","",.17, .15, .55, .55)
+inset.Draw()
+inset.cd()
+ins = ROOT.TH1F("ins","",1,700, 6200)
+ins.SetMaximum(10)
+ins.Draw()
+
+c.cd()
+
+leg = ROOT.TLegend(.5, .7, .80, .9)
 #leg.SetNColumns(3)
 leg.SetTextFont(42)
 leg.SetTextSize(0.032)
 leg.SetBorderSize(0)
 
+graphs = []
 for n,col in enumerate(mu_results):
 	graphs.append( ROOT.TGraph(len(mass), mass, col))
 	g = graphs[-1]
 	g.SetLineColor(colors[n])
 	g.SetLineWidth(3)
+	c.cd()
 	g.Draw("Lsame")
 	leg.AddEntry(g, mu_names[n], "L")
+	inset.cd()
+	g.Draw("Lsame")
 
+c.cd()
 leg.Draw("same")
-c.SetLogy()
 c.SaveAs("plots/mu_events.png")
 c.SaveAs("plots/mu_events.pdf")
 c.SaveAs("plots/mu_events.C")
-c.SetLogy(0)
-h.SetMaximum(mu_results.max()*1.1)
-h.SetMaximum(10)
-c.SaveAs("plots/mu_events_lin.png")
-c.SaveAs("plots/mu_events_lin.pdf")
-c.SaveAs("plots/mu_events_lin.C")
