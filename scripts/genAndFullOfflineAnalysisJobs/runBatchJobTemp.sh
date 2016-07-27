0a8cfcfe44f0e829077beea414671c7b26475e59
skalafut <kalaf004@umn.edu>
2016-07-27 17:22:53 +0200

    updated runBatchJobTemp.sh in genAndFullOfflineAnalysisJobs in scripts dir to no longer source a grid proxy which is not needed

--- scripts/genAndFullOfflineAnalysisJobs/runBatchJobTemp.sh
+++ scripts/genAndFullOfflineAnalysisJobs/runBatchJobTemp.sh
@@ -2,7 +2,7 @@
 #use this file with a command like
 #bsub -R "rusage[mem=8000]" -q 8nh -J jobName < scriptName.sh
 UP=LOCALPATH
-export X509_USER_PROXY=PROXYPATH
+#grid proxy no longer needed export X509_USER_PROXY=PROXYPATH
 
 cd $UP
 eval `scramv1 runtime -sh`
