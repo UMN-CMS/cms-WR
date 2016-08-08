7d7da0dc1134bbcc5375d3d0a601c8920f13f347
skalafut <kalaf004@umn.edu>
2016-08-08 11:16:10 +0200

    modified scripts/submitAnalysisForLimits.sh to run jobs on lxplus batch 2nd queue to process DY and WR MC in both channels with signal region requirements, 3200 toys, and all systematics enabled in analysis.cpp

--- scripts/submitAnalysisForLimits.sh
+++ scripts/submitAnalysisForLimits.sh
@@ -1,9 +1,9 @@
-arguments="--toys 410"
+arguments="--toys 3200"
 logdir=$PWD/log
 mass_n=$(seq 1 25)
 
-queue=1nh
-tag=410toysAllSyst
+queue=2nd
+tag=3200toysAllSyst
 finaldir=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/processedWithAnalysisCpp/$tag/
 
 for mass in $mass_n
@@ -13,8 +13,8 @@ do
 done
 
 #EMu data
-bsub -q $queue -eo $logdir/data_emu_ee_${tag}.err   -oo $logdir/data_emu_ee_${tag}.out   "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel EE\""
-bsub -q $queue -eo $logdir/data_emu_mumu_${tag}.err -oo $logdir/data_emu_mumu_${tag}.out "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel MuMu\""
+#bsub -q $queue -eo $logdir/data_emu_ee_${tag}.err   -oo $logdir/data_emu_ee_${tag}.out   "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel EE\""
+#bsub -q $queue -eo $logdir/data_emu_mumu_${tag}.err -oo $logdir/data_emu_mumu_${tag}.out "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel MuMu\""
 
 #EE dy
 bsub -q $queue -eo $logdir/dy_ee_${tag}.err -oo $logdir/dy_ee_${tag}.out "$PWD/runJob.sh EE DYAMC $finaldir $PWD \"$arguments\""
