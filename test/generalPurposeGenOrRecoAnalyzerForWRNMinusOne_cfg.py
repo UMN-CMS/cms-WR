539ec276ee6d7fa4004364ff4489ba0c2c256c26
skalafut <kalaf004@umn.edu>
2016-08-18 17:07:38 +0200

    updated generalPurposeGenOrRecoAnalyzerForWRNMinusOne_cfg.py

--- test/generalPurposeGenOrRecoAnalyzerForWRNMinusOne_cfg.py
+++ test/generalPurposeGenOrRecoAnalyzerForWRNMinusOne_cfg.py
@@ -10,6 +10,7 @@ process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load('ExoAnalysis.cmsWR.wrGenAndRecoForNMinusOne_cff')
 process.load('ExoAnalysis.cmsWR.ttBarGenAndRecoForNMinusOne_cff')
 process.GlobalTag.globaltag = '74X_mcRun2_asymptotic_v4'
 
@@ -40,61 +41,96 @@ matchedGenLeptonCollName="tempMatchedLeptColl"
 recoLeptCollName="tempRecoLeptColl"
 if (options.channel=='EE'):
 	genLeptonCollName = "bareGenEle"
-	matchedGenLeptonCollName = "matchedGenElectronFromTTBar"
+	matchedGenLeptonCollName = ""
 	recoLeptCollName = "heepRecoEle"
 #
 if (options.channel=='MuMu'):
 	genLeptonCollName = "bareGenMu"
-	matchedGenLeptonCollName = "matchedGenMuonFromTTBar"
+	matchedGenLeptonCollName = ""
 	recoLeptCollName = "highPtIdRecoMu"
-#
-
 #################################
 #Analyzers
 
 ## analyze the kinematic distributions of leading leptons and leading hadrons at gen or reco level with these analyzers
+
+#gen quarks and leptons without GEN mother requirements
 process.analyzerOne = cms.EDAnalyzer('generalPurposeGenAndRecoAnalyzer',
 		treeName = cms.string("genKinematicsUsingGenQuarksWithoutGenMotherRequirements"),
 		inputParticleCollsAreGEN = cms.bool(True),
 		inputHadronsAreQuarks = cms.bool(True),
-		hadronCollection = cms.InputTag("hadronCollName"),
-		leptonCollection = cms.InputTag("leptonCollName")
+		hadronCollection = cms.InputTag("bareGenQuark"),
+		leptonCollection = cms.InputTag(genLeptonCollName)
 		)
 
+#gen quarks and leptons with GEN mother requirements
 process.analyzerTwo = process.analyzerOne.clone(
 		treeName = cms.string("genKinematicsUsingGenQuarksWithGenMotherRequirements"),
-		hadronCollection = cms.InputTag("hadronCollName"),
-		leptonCollection = cms.InputTag("leptonCollName")
+		hadronCollection = cms.InputTag("matchedGenQuarkFromTTBar"),
+		leptonCollection = cms.InputTag(matchedGenLeptonCollName)
 		)
 
+#gen jets and leptons without GEN mother requirements
 process.analyzerThree = process.analyzerOne.clone(
 		treeName = cms.string("genKinematicsUsingGenJetsWithoutGenMotherRequirements"),
 		inputHadronsAreQuarks = cms.bool(False),
-		hadronCollection = cms.InputTag("hadronCollName")
+		hadronCollection = cms.InputTag("bareGenJet")
 		)
 
-process.analyzerFour = process.analyzerThree.clone(
+#gen jets and leptons with GEN mother requirements
+process.analyzerFour = process.analyzerTwo.clone(
 		treeName = cms.string("genKinematicsUsingGenJetsWithGenMotherRequirements"),
-		hadronCollection = cms.InputTag("hadronCollName"),
-		leptonCollection = cms.InputTag("leptonCollName")
+		inputHadronsAreQuarks = cms.bool(False),
+		hadronCollection = cms.InputTag("condenseMatchedGenJetCollName"),
 		)
 
+#reco jets and leptons, no matching requirements
 process.analyzerFive = cms.EDAnalyzer('generalPurposeGenAndRecoAnalyzer',
 		treeName = cms.string("recoKinematics"),
 		inputParticleCollsAreGEN = cms.bool(False),
 		inputHadronsAreQuarks = cms.bool(False),
-		hadronCollection = cms.InputTag("hadronCollName"),
-		leptonCollection = cms.InputTag("leptonCollName")
+		hadronCollection = cms.InputTag("bareRecoJetPassingId"),
+		leptonCollection = cms.InputTag(recoLeptCollName)
 		)
 
 #################################
 #Paths
-process.analyzeWRdecay = cms.Path(
-		process.genAndRecoFormatterSeq
-		*process.analyzerOne
-		)
+process.analyzeWRdecayGenQuarksWithoutMatching = cms.Path(process.analyzerOne)
+process.analyzeWRdecayGenQuarksWithMatching = cms.Path(process.analyzerTwo)
+process.analyzeWRdecayGenJetsWithoutMatching = cms.Path(process.analyzerThree)
+process.analyzeWRdecayGenJetsWithMatching = cms.Path(process.analyzerFour)
+process.analyzeWRdecayReco = cms.Path(process.analyzerFive)
 
-process.schedule = cms.Schedule(process.analyzeWRdecay)
+#all paths need to be modified based on the lepton channel
+if (options.channel=='EE'):
+	process.analyzeWRdecayReco = cms.Path(
+			process.egmGsfElectronIDSequence*process.HEEPIDSequence
+			*process.recoEleJetPassingIdSeq*process.analyzerFive
+			)
+	process.analyzeWRdecayGenQuarksWithoutMatching = cms.Path(process.genEleChnlGenQuarksNoMatchingSequence*process.analyzerOne)
+	process.analyzeWRdecayGenQuarksWithMatching = cms.Path(process.matchedGenEleAndQuarkFromTTBarSeq*process.analyzerTwo)
+	process.analyzeWRdecayGenJetsWithoutMatching = cms.Path(process.genEleChnlGenJetsNoMatchingSequence*process.analyzerThree)
+	process.analyzeWRdecayGenJetsWithMatching = cms.Path(
+			process.matchedGenEleAndQuarkFromTTBarSeq
+			*process.genJetSeq
+			*process.matchedGenJetSeq
+			*process.condenseGenJetTTBarSeq
+			*process.analyzerFour)
+
+#
+if (options.channel=='MuMu'):
+	process.analyzeWRdecayReco = cms.Path(process.recoMuJetPassingIdSeq*process.analyzerFive)
+	process.analyzeWRdecayGenQuarksWithoutMatching = cms.Path(process.genMuChnlGenQuarksNoMatchingSequence*process.analyzerOne)
+	process.analyzeWRdecayGenQuarksWithMatching = cms.Path(process.matchedGenMuAndQuarkFromTTBarSeq*process.analyzerTwo)
+	process.analyzeWRdecayGenJetsWithoutMatching = cms.Path(process.genMuChnlGenJetsNoMatchingSequence*process.analyzerThree)
+	process.analyzeWRdecayGenJetsWithMatching = cms.Path(
+			process.matchedGenMuAndQuarkFromTTBarSeq
+			*process.genJetSeq
+			*process.matchedGenJetSeq
+			*process.condenseGenJetTTBarSeq
+			*process.analyzerFour)
+
+#
+#process.schedule = cms.Schedule(process.analyzeWRdecay)
 
 
 process.TFileService = cms.Service("TFileService",
@@ -112,14 +148,15 @@ inputFiles = cms.untracked.vstring(options.files)
 process.source = cms.Source( "PoolSource",
 	fileNames = inputFiles,
 	#fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/miniAOD/WR_signal_MC/WRToNuEToEEJJ_MW-6000_MNu-3000_TuneCUETP8M1_pythia8_13TeV.root'),
+	#fileNames = cms.untracked.vstring('/store/mc/RunIISpring15MiniAODv2/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2_ext1-v1/10000/0036F7A6-CC6D-E511-8B5B-002590D60150.root')
 	#inputCommands = cms.untracked.vstring(
     #    'keep *'
     #)
 )
 
 process.maxEvents = cms.untracked.PSet(
-    #input = cms.untracked.int32(options.maxEvents)
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(options.maxEvents)
+    #input = cms.untracked.int32(-1)
 )
 
 
