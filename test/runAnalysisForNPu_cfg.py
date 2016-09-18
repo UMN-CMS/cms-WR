02c8d4101c4fe3ee5c1c759418395a9636d6ad3b
skalafut <kalaf004@umn.edu>
2016-09-19 01:43:28 +0200

    commented out maxEvents default value in runAnalysisForNPu_cfg.py when test equals 2

--- test/runAnalysisForNPu_cfg.py
+++ test/runAnalysisForNPu_cfg.py
@@ -53,7 +53,7 @@ if(options.test==3):
 elif(options.test==2):
 	options.files="file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_1.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_2.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_3.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_4.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_5.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_6.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_7.root","file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_8.root"
 	#options.files="file:/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveM50MiniAodSim_1.root"
-	options.maxEvents= -1
+	#options.maxEvents= -1
 	options.isMC=1
 	options.datasetTag='dyJetsAmcNloInclusiveNoSelectionCutsForPUweights'
 	#options.output = "/afs/cern.ch/work/s/skalafut/public/WR_starting2015/puReweightingFiles/dyJetsAmcNloInclusiveWithNumPUandNoSelections.root"
