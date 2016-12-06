96cda083b405508d64cb59dffb72c2429f4dd524
skalafut <kalaf004@umn.edu>
2016-12-06 19:46:03 +0100

    modified dilepton mass cut in analysis.cpp and file read path in configReader.cc to process custom muEG flavoursideband minitree. fixup

--- src/configReader.cc
+++ src/configReader.cc
@@ -60,7 +60,8 @@ configReader::configReader(std::string filename)
 TChain *configReader::getMiniTreeChain(std::string datasetName, std::string tag)
 {
 	TChain *chain = new TChain((tag + "/t").c_str(), "");
-	chain->Add(("root://eoscms.cern.ch//store/user/shervin/ntuples/" + datasetName + configFile["productionTAG"] + unblindTag() + "/unmerged-allRange.root").c_str());
+	//chain->Add(("root://eoscms.cern.ch//store/user/shervin/ntuples/" + datasetName + configFile["productionTAG"] + unblindTag() + "/unmerged-allRange.root").c_str());
+	chain->Add("/afs/cern.ch/work/s/skalafut/public/WR_starting2015/custom2015Minitrees/muEGminitreesWithLowDileptonReqInFlavourSideband.root");
 	chain->GetEntries();
 	return chain;
 }
