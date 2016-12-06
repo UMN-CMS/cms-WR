02023859c8df4066a6511aa2868338148cf30a8f
skalafut <kalaf004@umn.edu>
2016-12-06 16:56:49 +0100

    added runAnalysisMuEG.sh for muEG minitree, modified runAnalysisTTMC.sh and switched TTMC file order in runAnalysis_cfg.py

--- runAnalysisMuEGData.sh
+++ runAnalysisMuEGData.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #use this file with a command like
-#bsub -R "pool>8000" -q 1nd -J runAnalysisTTMC < runAnalysisTTMC.sh
+#bsub -R "rusage[mem=8000]" -q 1nd -J runAnalysisMuEGData < runAnalysisMuEGData.sh
 CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
 UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment
 export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
 
 cd $UP/$CMSSW_PROJECT_SRC
 eval `scramv1 runtime -sh`
-eval "cmsRun test/runAnalysis_cfg.py test=2"
+eval "cmsRun test/runAnalysis_cfg.py test=0 output=reprocessMuEGDataMinitrees.root >& MuEGDataOutput.txt &"
 
