ea37de17b270a003d3d3cea0ab2cf3389e970ff4
skalafut <kalaf004@umn.edu>
2016-12-06 18:35:06 +0100

    updated runAnalysisMuEGData.sh and runAnalysis_cfg.py to make flavoursideband minitree from data with low dilepton mass req

--- runAnalysisMuEGData.sh
+++ runAnalysisMuEGData.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #use this file with a command like
-#bsub -R "rusage[mem=8000]" -q 1nd -J runAnalysisMuEGData < runAnalysisMuEGData.sh
+#bsub -R "pool>8000" -q 1nd -J runAnalysisMuEGData < runAnalysisMuEGData.sh
 CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
-UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment
+UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment/onlyForTwoDimLimitJobs
 export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
 
 cd $UP/$CMSSW_PROJECT_SRC
 eval `scramv1 runtime -sh`
-eval "cmsRun test/runAnalysis_cfg.py test=0 output=reprocessMuEGDataMinitrees.root >& MuEGDataOutput.txt &"
+eval "cmsRun test/runAnalysis_cfg.py test=0 jsonFile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt"
 
