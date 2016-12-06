0b77fa401db36786c09d3da66a9d0ea87767ccde
skalafut <kalaf004@umn.edu>
2016-12-06 17:45:53 +0100

    only run muon and electron systematics

--- scripts/submitAnalysisForLimits.sh
+++ scripts/submitAnalysisForLimits.sh
@@ -1,29 +1,37 @@
-arguments="--toys 4000 --makeSelectorPlots"
+arguments="--toys 3200"
 logdir=$PWD/log
 mass_n=$(seq 1 25)
 
-queue=1nd
-tag=4000toys
-finaldir=/afs/cern.ch/work/s/shervin/public/$tag/
+#queue=2nd
+queue=1nw
+tag=3200toysEleMuScaleSmearingSyst
+finaldir=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/processedWithAnalysisCpp/$tag/
 
-for mass in $mass_n
-do
-	bsub -q $queue -eo $logdir/${mass}_signal_ee_${tag}.err -oo $logdir/${mass}_signal_ee_${tag}.out "$PWD/runJob.sh EE signal $finaldir $PWD \"$arguments --signalN $mass\""
-	bsub -q $queue -eo $logdir/${mass}_signal_mumu_${tag}.err -oo $logdir/${mass}_signal_mumu_${tag}.out "$PWD/runJob.sh MuMu signal $finaldir $PWD \"$arguments --signalN $mass\""
-done
+#for mass in $mass_n
+#do
+#	bsub -q $queue -eo $logdir/${mass}_signal_ee_${tag}.err -oo $logdir/${mass}_signal_ee_${tag}.out "$PWD/runJob.sh EE signal $finaldir $PWD \"$arguments --signalN $mass\""
+#	bsub -q $queue -eo $logdir/${mass}_signal_mumu_${tag}.err -oo $logdir/${mass}_signal_mumu_${tag}.out "$PWD/runJob.sh MuMu signal $finaldir $PWD \"$arguments --signalN $mass\""
+#done
 
 #EMu data
-bsub -q $queue -eo $logdir/data_emu_ee_${tag}.err   -oo $logdir/data_emu_ee_${tag}.out   "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel EE\""
-bsub -q $queue -eo $logdir/data_emu_mumu_${tag}.err -oo $logdir/data_emu_mumu_${tag}.out "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel MuMu\""
+#bsub -q $queue -eo $logdir/data_emu_ee_${tag}.err   -oo $logdir/data_emu_ee_${tag}.out   "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel EE\""
+#bsub -q $queue -eo $logdir/data_emu_mumu_${tag}.err -oo $logdir/data_emu_mumu_${tag}.out "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel MuMu\""
 
 #EE dy
-bsub -q $queue -eo $logdir/dy_ee_${tag}.err -oo $logdir/dy_ee_${tag}.out "$PWD/runJob.sh EE DYAMC $finaldir $PWD \"$arguments\""
-
-#MUMU dy
-bsub -q $queue -eo $logdir/dy_mumu_${tag}.err -oo $logdir/dy_mumu_${tag}.out "$PWD/runJob.sh MuMu DYAMC $finaldir $PWD \"$arguments\""
-
-##EE ttbar mc
+#bsub -q $queue -eo $logdir/dy_ee_${tag}.err -oo $logdir/dy_ee_${tag}.out "$PWD/runJob.sh EE DYAMC $finaldir $PWD \"$arguments\""
+#
+##MUMU dy
+#bsub -q $queue -eo $logdir/dy_mumu_${tag}.err -oo $logdir/dy_mumu_${tag}.out "$PWD/runJob.sh MuMu DYAMC $finaldir $PWD \"$arguments\""
+#
+###EE ttbar mc
 bsub -q $queue -eo $logdir/ttbar_ee_${tag}.err -oo $logdir/ttbar_ee_${tag}.out "$PWD/runJob.sh EE TT $finaldir $PWD \"$arguments\""
-
-##MUMU ttbar mc
+##
+####MUMU ttbar mc
 bsub -q $queue -eo $logdir/ttbar_mumu_${tag}.err -oo $logdir/ttbar_mumu_${tag}.out "$PWD/runJob.sh MuMu TT $finaldir $PWD \"$arguments\""
+#
+###EMu ttbar mc with MuMu mass windows
+bsub -q $queue -eo $logdir/ttbar_emu_mumucut_${tag}.err -oo $logdir/ttbar_emu_mumucut_${tag}.out "$PWD/runJob.sh EMu TT $finaldir $PWD \"$arguments  --cut_channel MuMu\""
+
+###EMu ttbar mc with EE mass windows
+bsub -q $queue -eo $logdir/ttbar_emu_eecut_${tag}.err -oo $logdir/ttbar_emu_eecut_${tag}.out "$PWD/runJob.sh EMu TT $finaldir $PWD \"$arguments  --cut_channel EE\""
+
