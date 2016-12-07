09dec95d64bc7f91bdc324fe516596207829bf5a
skalafut <kalaf004@umn.edu>
2016-12-07 15:37:35 +0100

    modified runAnalysisTTMC.sh and runAnalysis_cfg.py before running multiple batch jobs to process TTJets v2 MC

--- runAnalysisTTMC.sh
+++ runAnalysisTTMC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #use this file with a command like
-#bsub -R "pool>8000" -q 1nd -J runAnalysisTTMC < runAnalysisTTMC.sh
+#bsub -R "pool>8000" -q 8nh -J runAnalysisTTMC < runAnalysisTTMC.sh
 CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
 UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment
 export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
