b060eb997b0c58d00fabb4c291303a6d365739ae
skalafut <kalaf004@umn.edu>
2016-12-19 16:02:44 +0100

    added arg to submitAnalysisForLimits.sh to include DY normalization factors when running with toys

--- scripts/submitAnalysisForLimits.sh
+++ scripts/submitAnalysisForLimits.sh
@@ -1,4 +1,4 @@
-arguments="--toys 3200"
+arguments="--toys 3200 --ignoreDyScaleFactors false"
 logdir=$PWD/log
 mass_n=$(seq 1 25)
 
@@ -16,11 +16,18 @@ done
 #bsub -q $queue -eo $logdir/data_emu_ee_${tag}.err   -oo $logdir/data_emu_ee_${tag}.out   "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel EE\""
 #bsub -q $queue -eo $logdir/data_emu_mumu_${tag}.err -oo $logdir/data_emu_mumu_${tag}.out "$PWD/runJob.sh EMu data $finaldir $PWD \"$arguments  --cut_channel MuMu\""
 
-#EE dy
+#DYAMC EE dy
 bsub -q $queue -eo $logdir/dy_ee_${tag}.err -oo $logdir/dy_ee_${tag}.out "$PWD/runJob.sh EE DYAMC $finaldir $PWD \"$arguments\""
 
-#MUMU dy
+#DYAMC MUMU dy
 bsub -q $queue -eo $logdir/dy_mumu_${tag}.err -oo $logdir/dy_mumu_${tag}.out "$PWD/runJob.sh MuMu DYAMC $finaldir $PWD \"$arguments\""
+
+#DYMADHT EE dy
+bsub -q $queue -eo $logdir/dy_ee_${tag}.err -oo $logdir/dy_ee_${tag}.out "$PWD/runJob.sh EE DYMADHT $finaldir $PWD \"$arguments\""
+
+#DYMADHT MUMU dy
+bsub -q $queue -eo $logdir/dy_mumu_${tag}.err -oo $logdir/dy_mumu_${tag}.out "$PWD/runJob.sh MuMu DYMADHT $finaldir $PWD \"$arguments\""
+
 
 ##EE ttbar mc
 #bsub -q $queue -eo $logdir/ttbar_ee_${tag}.err -oo $logdir/ttbar_ee_${tag}.out "$PWD/runJob.sh EE TT $finaldir $PWD \"$arguments\""
