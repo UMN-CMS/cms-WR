d286d7b6210d40bf23399aabe4ae1ef136a6e671
skalafut <kalaf004@umn.edu>
2017-01-24 15:20:52 +0100

    rewrote script to submit skim jobs to crab. added script to check status of crab jobs

---
+++ checkCrabStatuses.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#path names to job directories
+jobDirs=('crab/skim/crab_skim_QCD_emenr_pt120to170_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt15to20_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt170to300_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt20to30_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt300toInf_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt30to50_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt50to80_SHv12' 'crab/skim/crab_skim_QCD_emenr_pt80to120_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt1000toInf_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt120to170_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt15to20_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt170to300_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt20to30_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt300to470_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt30to50_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt470to600_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt50to80_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt600to800_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt800to1000_SHv12' 'crab/skim/crab_skim_QCD_muenr_pt80to120_SHv12')
+subDirs=('crab_QCD_emenr_pt120to170' 'crab_QCD_emenr_pt15to20' 'crab_QCD_emenr_pt170to300' 'crab_QCD_emenr_pt20to30' 'crab_QCD_emenr_pt300toInf' 'crab_QCD_emenr_pt30to50' 'crab_QCD_emenr_pt50to80' 'crab_QCD_emenr_pt80to120' 'crab_QCD_muenr_pt1000toInf' 'crab_QCD_muenr_pt120to170' 'crab_QCD_muenr_pt15to20' 'crab_QCD_muenr_pt170to300' 'crab_QCD_muenr_pt20to30' 'crab_QCD_muenr_pt300to470' 'crab_QCD_muenr_pt30to50' 'crab_QCD_muenr_pt470to600' 'crab_QCD_muenr_pt50to80' 'crab_QCD_muenr_pt600to800' 'crab_QCD_muenr_pt800to1000' 'crab_QCD_muenr_pt80to120')
+space='  '
+
+for i in ${!jobDirs[*]}
+do
+	eval "crab status -d "
+	#echo "crab status -d ${jobDirs[$i]}/${subDirs[$i]}"
+	eval "echo $space"
+done
+
---
+++ scripts/simplerMakeAndRunCrabSkimScripts.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+###### the following variables are defined in a separate config file
+# datasetFile=configs/datasets.dat
+# jsonFile=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt
+# jsonName=246908-260627-Prompt_25ns-golden_silver-v1
+source configs/2015-v1.conf
+
+
+crabFile=tmp/crab.py
+
+#pull the dataset path names and short names from a txt file
+datasets=(`cat $datasetFile | grep -v '#' | awk '{print $2}'`)
+datasetNames=(`cat $datasetFile | grep -v '#' | awk '{print $1}'`)
+
+#make a tmp directory if one does not yet exist
+if [ ! -d "tmp/" ];then mkdir tmp/; fi
+
+for i in `seq 0 ${#datasets[@]}`
+do
+	params=""
+	dataset=${datasets[${i}]}
+	datasetName=${datasetNames[${i}]}
+	if [ -z "${datasetName}" ];then continue; fi
+	echo $i $datasetName
+	#echo $dataset
+	crabFile=tmp/skim_$datasetName.py
+
+	cat > $crabFile  <<EOF
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = "$datasetName"
+config.General.workArea = 'crab/skim/crab_skim_'+"$datasetName${skimProductionTAG}"
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'test/skims_cfg.py'
+EOF
+
+LUMI=2000
+#### if the dataset is DATA or DY save the TagAndProbe triggers
+case $datasetName in 
+	DoubleEG*)
+		params="$params, 'saveTnP=1', 'GT=74X_dataRun2_Prompt_v5'"
+		LUMI=1000
+		;;
+	SingleMu*)
+		params="$params, 'saveTnP=1', 'GT=74X_dataRun2_Prompt_v5'"
+		LUMI=1000
+		;;
+	MuEG*)
+		params="$params, 'saveTnP=0', 'GT=74X_dataRun2_Prompt_v5'"
+		LUMI=1000
+		;;
+	DY*)
+		params="$params, 'saveTnP=1', 'GT=74X_mcRun2_asymptotic_v4'"
+		jsonFile=""
+		;;
+	
+	*)
+		params="$params, 'saveTnP=0', 'GT=74X_mcRun2_asymptotic_v4'"
+		jsonFile=""
+		;;
+esac
+params=`echo $params | sed -r 's|^,||;s|[,]+|,|g'`
+
+cat >> $crabFile <<EOF
+config.JobType.pyCfgParams = [ $params ]
+#config.JobType.maxMemoryMB = 2500 should not need this option for skims
+
+config.Data.inputDataset = "$dataset"
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased'
+config.Data.lumiMask = 'LUMI'
+config.Data.unitsPerJob = ${LUMI}
+
+#True allows the jobs to run anywhere, regardless of where the input data is located
+config.Data.ignoreLocality = False
+
+#totalUnits only needs to be specified for GEN-SIM jobs
+#config.Data.totalUnits = 200000
+config.Data.outLFNDirBase = '/store/group/phys_exotica/leptonsPlusJets/WR/skims/'
+config.Data.publication = True 
+#config.Data.publishDataName = 'realData_FNLST_13TeV_CHNL_UNIQUE'
+config.Data.outputDatasetTag =  config.General.requestName + "${skimProductionTAG}"
+config.Data.lumiMask = "$jsonFile"
+
+#a list of the only sites at which these jobs can run
+#config.Site.whitelist = ["T2_US*"]
+#config.Site.storageSite = 'T3_US_FNALLPC'
+config.Site.storageSite = 'T2_CH_CERN'
+
+EOF
+
+
+#echo "crab submit -c $crabFile"
+eval "crab submit -c $crabFile"
+
+
+done
+#skim_realData_${channel[$q]}_${datasets[$q]}_${suffix[$q]}.py"
+
+
+
