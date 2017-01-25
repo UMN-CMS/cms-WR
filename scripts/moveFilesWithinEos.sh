e04f4027801802a4be10e9ea4c50de63148a8c43
skalafut <kalaf004@umn.edu>
2017-01-25 21:40:08 +0100

    used eos move file script to delete eos dirs no longer needed

--- scripts/moveFilesWithinEos.sh
+++ scripts/moveFilesWithinEos.sh
@@ -21,34 +21,34 @@ newDirsToMake=(`cat $datasetFile | grep -v '#' | awk '{print $1}'`)
 #
 #done
 
-cpTag='root://eoscms.cern.ch/'
-for i in ${!topLevelDirs[*]}
-do
-	#get the list of subdirectories and files within one skim directory, like WW or QCD EMEnriched pt30to50
-	#element i in topLevelDirs corresponds to element i in newDirsToMake, which have already been made
-	#all elements in skimFiles are root files which were selected by the skims
-	skimFiles=(`eos find $eosSkimPath${topLevelDirs[$i]} | grep output`)
+#cpTag='root://eoscms.cern.ch/'
+#for i in ${!topLevelDirs[*]}
+#do
+#	#get the list of subdirectories and files within one skim directory, like WW or QCD EMEnriched pt30to50
+#	#element i in topLevelDirs corresponds to element i in newDirsToMake, which have already been made
+#	#all elements in skimFiles are root files which were selected by the skims
+#	skimFiles=(`eos find $eosSkimPath${topLevelDirs[$i]} | grep output`)
+#
+#	for k in ${!skimFiles[*]}
+#	do
+#		let y=k+1
+#		#RUN this echo command and comment out code further below before trying to actually cp any files between dirs on eos.
+#		#make sure the dir name in the skimFiles element matches with the element in newDirsToMake, otherwise
+#		#a skim from one dataset will end up in a skim of a different dataset
+#		#echo "xrdcp $cpTag${skimFiles[$k]} ."
+#		#echo "xrdcp output_${y}.root $cpTag$eosSkimPath${newDirsToMake[$i]}$skimProductionTAG/"
+#		#echo "rm output_${y}.root"
+#
+#		#copy each element from the original dir to the local dir, and the local dir to the new dir
+#		eval "xrdcp $cpTag${skimFiles[$k]} ."
+#		eval "xrdcp output_${y}.root $cpTag$eosSkimPath${newDirsToMake[$i]}$skimProductionTAG/"
+#		eval "rm output_${y}.root"
+#
+#	done
+#
+#done
 
-	for k in ${!skimFiles[*]}
-	do
-		let y=k+1
-		#RUN this echo command and comment out code further below before trying to actually cp any files between dirs on eos.
-		#make sure the dir name in the skimFiles element matches with the element in newDirsToMake, otherwise
-		#a skim from one dataset will end up in a skim of a different dataset
-		#echo "xrdcp $cpTag${skimFiles[$k]} ."
-		#echo "xrdcp output_${y}.root $cpTag$eosSkimPath${newDirsToMake[$i]}$skimProductionTAG/"
-		#echo "rm output_${y}.root"
-
-		#copy each element from the original dir to the local dir, and the local dir to the new dir
-		eval "xrdcp $cpTag${skimFiles[$k]} ."
-		eval "xrdcp output_${y}.root $cpTag$eosSkimPath${newDirsToMake[$i]}$skimProductionTAG/"
-		eval "rm output_${y}.root"
-
-	done
-
-done
-
-##remove the original directory on eos where the skim output files were stored
+#remove the original directory on eos where the skim output files were stored
 #for q in ${!topLevelDirs[*]}
 #do
 #	#RUN the echo command before actually removing any directory
