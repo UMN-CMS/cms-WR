f626fa72e84198a1856b0ac73c66b2c592ad7463
skalafut <kalaf004@umn.edu>
2017-01-30 17:12:47 +0100

    enabled pdfWeight usage and added new mode to analysis.cpp to process low HT DYMadIncl and HT binned DYMad samples in one job

--- bin/analysis.cpp
+++ bin/analysis.cpp
@@ -59,7 +59,7 @@ class chainNames
 public:
 	chainNames(): ///< default constructor
 		all_modes(  // list of all possible modes
-	{"TT", "W", "WZ", "ZZ", "data", "DYPOWHEG", "DYMADHT", "DYAMC", "DYMAD", "DYPOWINCL", "singleTop", "topW", "WW", "QCD", "signal"
+	{"TT", "W", "WZ", "ZZ", "data", "DYPOWHEG", "DYMADHT", "DYAMC", "DYMAD", "DYMadInclAndHT", "DYPOWINCL", "singleTop", "topW", "WW", "QCD", "signal"
 	}
 	)
 	{
@@ -105,6 +105,12 @@ public:
 			} else if(mode.find("AMC") != _ENDSTRING) {
 				//amc at nlo inclusive sample gen dilepton mass greater than 50 GeV
 				TTchainNames.push_back("DYJets_amctnlo");
+			} else if(mode == "DYMadInclAndHT") {
+				TTchainNames.push_back("DYJets_madgraph");
+				TTchainNames.push_back("DYJets_madgraph_ht100to200");
+				TTchainNames.push_back("DYJets_madgraph_ht200to400");
+				TTchainNames.push_back("DYJets_madgraph_ht400to600");
+				TTchainNames.push_back("DYJets_madgraph_ht600toInf");
 			} else if(mode.find("MADHT") != _ENDSTRING) {
 				TTchainNames.push_back("DYJets_madgraph_ht100to200");
 				TTchainNames.push_back("DYJets_madgraph_ht200to400");
@@ -663,7 +669,7 @@ int main(int ac, char* av[])
 						//selEvent.weight *= wgt; 
 						selEvent.weight *= myReader.getNorm1fb(selEvent.datasetName) * integratedLumi; // the weight is the event weight * single object weights
 						if(mode.find("DY") != _ENDSTRING && pdfWeight != -1){
-							//selEvent.weight *= (*myEventIt.renormFactAndPdfWeights)[9+pdfWeight];
+							selEvent.weight *= (*myEventIt.renormFactAndPdfWeights)[9+pdfWeight];
 						}
 #ifdef DEBUGG
 						std::cout << "PU weight=\t" << selEvent.pu_weight << std::endl;
@@ -711,7 +717,7 @@ int main(int ac, char* av[])
 						selEvent.weight *= myReader.getNorm1fb(selEvent.datasetName) * integratedLumi; // the weight is the event weight * single object weights
 						
 						if(mode.find("DY") != _ENDSTRING && pdfWeight != -1){
-							//selEvent.weight *= (*myEventIt.renormFactAndPdfWeights)[9+pdfWeight];
+							selEvent.weight *= (*myEventIt.renormFactAndPdfWeights)[9+pdfWeight];
 						}
 
 						//multiply by an additional weight when processing DY samples
