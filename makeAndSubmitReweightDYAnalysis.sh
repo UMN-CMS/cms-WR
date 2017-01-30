30c9b597d862e7327aa2ff99b9855e34857c594e
skalafut <kalaf004@umn.edu>
2017-01-30 18:11:44 +0100

    updated reweight DYMC analysis scripts to process DYMadInclAndHT samples with different pdf weights

--- makeAndSubmitReweightDYAnalysis.sh
+++ makeAndSubmitReweightDYAnalysis.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+##ONLY run this script in cmsWR/ directory, not a subdirectory
+workingDir=$PWD
 
 ##AMC
-mode='DYAMC'
+#mode='DYAMC'
 
-##MadIncl LowHT
-#mode='DYMAD'
+##MadIncl LowHT and MadHT all bins
+mode='DYMadInclAndHT'
 
-##MadHT all bins
-#mode='DYMADHT'
 
 for i in {0..100}
 do
 	#replace WGTNUM by an integer, MODE by a string, and CHNL by a string
 	eval "sed 's@WGTNUM@$i@g' runAnalysisReweightDYMC.sh > tempOne.sh"
-	eval "sed 's@MODE@$mode@g' tempOne.sh > tempTwo.sh"
+	eval "sed 's@WORKDIR@$workingDir@g' tempOne.sh > tempOnePfive.sh"
+	eval "sed 's@MODE@$mode@g' tempOnePfive.sh > tempTwo.sh"
 	eval "sed 's@CHNL@EE@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
 	eval "sed 's@CHNL@MuMu@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
 	eval "chmod u+x runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
 	eval "chmod u+x runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-	rm tempOne.sh tempTwo.sh
+	rm tempOne.sh tempOnePfive.sh tempTwo.sh
 
 	#submit the job(s)
-	#echo "bsub -R 'pool>2000' -q 8nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-	#echo "bsub -R 'pool>2000' -q 8nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-	eval "bsub -R 'pool>2000' -q 8nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-	eval "bsub -R 'pool>2000' -q 8nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
+	#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
+	#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
+	eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
+	eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
 	
 done
 
--- runAnalysisReweightDYMC.sh
+++ runAnalysisReweightDYMC.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #use this file with a command like
-#bsub -R "pool>2000" -q 8nh -J uniqueJobName_WGTNUM < runAnalysisReweightDYMC_MODE_CHNL_WGTNUM.sh
-CMSSW_PROJECT_SRC=CMSSW_7_4_15_patch1/src/ExoAnalysis/cmsWR
-UP=/afs/cern.ch/work/s/skalafut/public/WR_starting2015/wrDevelopment
-#export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
+#bsub -R "pool>1500" -q 1nh -J uniqueJobName_WGTNUM < runAnalysisReweightDYMC_MODE_CHNL_WGTNUM.sh
+export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
 
-cd $UP/$CMSSW_PROJECT_SRC
+cd WORKDIR
 eval `scramv1 runtime -sh`
 eval "make"
-#output file size is too large with tagandprobe eval "./bin/analysis -m MODE -c CHNL --isTagAndProbe true --pdfWeight WGTNUM"
-#eval "./bin/analysis -m MODE -c CHNL --ignoreDyScaleFactors false --isLowDiLepton true --pdfWeight WGTNUM"
 eval "./bin/analysis -m MODE -c CHNL --ignoreDyScaleFactors false --pdfWeight WGTNUM"
 
