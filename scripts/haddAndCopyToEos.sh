8e358418683737c489ac411a359edc0571b8bd2d
skalafut <kalaf004@umn.edu>
2017-01-31 16:45:25 +0100

    modified hadd and copy to EOS script to make dirs for WR minitrees, and move new minitrees to those dirs

--- scripts/haddAndCopyToEos.sh
+++ scripts/haddAndCopyToEos.sh
@@ -6,13 +6,20 @@ source configs/2015-v1.conf
 #all input files to process
 ##for all datasets in configs/missing_datasets.dat
 ##first get all the dataset short names, and save them to a list
-mcIdentifier=(`cat $datasetFile | grep -v '#' | awk '{print $1}'`)
+
+#for bkgnd minitrees
+#mcIdentifier=(`cat $datasetFile | grep -v '#' | awk '{print $1}'`)
+
+#for WR minitrees
+mcIdentifier=(`cat $datasetFile | grep -v '#' | grep 'WR' | awk '{print $1}'`)
+
 eosTuplePath='/eos/cms/store/group/phys_exotica/leptonsPlusJets/WR/tuples/'
 eosReadingTag='root://eoscms.cern.ch/'
 
 #now loop over all datasets
 for j in ${!mcIdentifier[*]}
 do
+	#eos mkdir -p $eosTuplePath${mcIdentifier[$j]}$productionTAG
 	hadd -k -O unmerged-allRange.root ${mcIdentifier[$j]}Part*.root
 	xrdcp unmerged-allRange.root $eosReadingTag$eosTuplePath${mcIdentifier[$j]}$productionTAG/.
 	rm unmerged-allRange.root
