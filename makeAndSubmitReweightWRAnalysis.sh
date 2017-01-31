57da3615c27bbd3ced41504df437b11cbe4b403a
skalafut <kalaf004@umn.edu>
2017-01-31 17:25:25 +0100

    finished scripts for processing WR minitrees with pdf weights

--- makeAndSubmitReweightWRAnalysis.sh
+++ makeAndSubmitReweightWRAnalysis.sh
@@ -2,59 +2,32 @@
 ##ONLY run this script in cmsWR/ directory, not a subdirectory
 workingDir=$PWD
 
-##AMC
-#mode='DYAMC'
-
-##MadIncl LowHT and MadHT all bins
-#mode='DYMadInclAndHT'
-
-##for DYMAD and DYAMC
-#for i in {0..100}
-#do
-#	#replace WGTNUM by an integer, MODE by a string, and CHNL by a string
-#	eval "sed 's@WGTNUM@$i@g' runAnalysisReweightDYMC.sh > tempOne.sh"
-#	eval "sed 's@WORKDIR@$workingDir@g' tempOne.sh > tempOnePfive.sh"
-#	eval "sed 's@MODE@$mode@g' tempOnePfive.sh > tempTwo.sh"
-#	eval "sed 's@CHNL@EE@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-#	eval "sed 's@CHNL@MuMu@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-#	eval "chmod u+x runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-#	eval "chmod u+x runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-#	rm tempOne.sh tempOnePfive.sh tempTwo.sh
-#
-#	#submit the job(s)
-#	#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-#	#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-#	eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-#	eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-#	
-#done
-
-
 ##WR signal
 mode='signal'
 mass_n=$(seq 1 25)
 
-#for WR signal
 for j in $mass_n
 do
 
+	#default max is 100
 	for i in {0..100}
 	do
-		#replace WGTNUM by an integer, MODE by a string, and CHNL by a string
-		eval "sed 's@WGTNUM@$i@g' runAnalysisReweightDYMC.sh > tempOne.sh"
-		eval "sed 's@WORKDIR@$workingDir@g' tempOne.sh > tempOnePfive.sh"
+		#replace WGTNUM by an integer, MODE by a string, WORKDIR by abs path, MMAASS by an integer, and CHNL by a string
+		eval "sed 's@WGTNUM@$i@g' runAnalysisReweightWRMC.sh > tempOne.sh"
+		eval "sed 's@MMAASS@$j@g' tempOne.sh > tempM.sh"
+		eval "sed 's@WORKDIR@$workingDir@g' tempM.sh > tempOnePfive.sh"
 		eval "sed 's@MODE@$mode@g' tempOnePfive.sh > tempTwo.sh"
-		eval "sed 's@CHNL@EE@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-		eval "sed 's@CHNL@MuMu@g' tempTwo.sh > runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-		eval "chmod u+x runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-		eval "chmod u+x runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-		rm tempOne.sh tempOnePfive.sh tempTwo.sh
+		eval "sed 's@CHNL@EE@g' tempTwo.sh > runAnalysisReweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i}.sh"
+		eval "sed 's@CHNL@MuMu@g' tempTwo.sh > runAnalysisReweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i}.sh"
+		eval "chmod u+x runAnalysisReweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i}.sh"
+		eval "chmod u+x runAnalysisReweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i}.sh"
+		rm tempOne.sh tempOnePfive.sh tempTwo.sh tempM.sh
 
 		#submit the job(s)
-		#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-		#echo "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
-		eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_EE_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_EE_pdfWeight_${i}.sh"
-		eval "bsub -R 'pool>1500' -q 1nh -J reweightDYMC_${mode}_MuMu_pdfWeight_${i} < runAnalysisReweightDYMC_${mode}_MuMu_pdfWeight_${i}.sh"
+		#echo "bsub -R 'pool>1100' -q 1nh -J reweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i} < runAnalysisReweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i}.sh"
+		#echo "bsub -R 'pool>1100' -q 1nh -J reweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i} < runAnalysisReweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i}.sh"
+		eval "bsub -R 'pool>1100' -q 1nh -J reweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i} < runAnalysisReweightWRMC_${mode}_signalMass_${j}_EE_pdfWeight_${i}.sh"
+		eval "bsub -R 'pool>1100' -q 1nh -J reweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i} < runAnalysisReweightWRMC_${mode}_signalMass_${j}_MuMu_pdfWeight_${i}.sh"
 
 	done
 
--- runAnalysisReweightWRMC.sh
+++ runAnalysisReweightWRMC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #use this file with a command like
-#bsub -R "pool>1500" -q 1nh -J uniqueJobName_WGTNUM < runAnalysisReweightDYMC_MODE_CHNL_WGTNUM.sh
+#bsub -R "pool>1500" -q 1nh -J uniqueJobName_WGTNUM < runAnalysisReweightWRMC_MODE_CHNL_WGTNUM.sh
 export X509_USER_PROXY=/afs/cern.ch/user/s/skalafut/x509up_u38430
 
 cd WORKDIR
 eval `scramv1 runtime -sh`
 eval "make"
-eval "./bin/analysis -m MODE -c CHNL --ignoreDyScaleFactors false --pdfWeight WGTNUM"
+eval "./bin/analysis -m MODE -c CHNL --pdfWeight WGTNUM --signalN MMAASS"
 
