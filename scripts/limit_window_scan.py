a944a2b37725566de752f44430ec443f1684e303
skalafut <kalaf004@umn.edu>
2017-02-10 19:08:44 +0100

    limit_window_scan.py works, limits are calculated

--- scripts/limit_window_scan.py
+++ scripts/limit_window_scan.py
@@ -1,15 +1,14 @@
 import subprocess
 import ExoAnalysis.cmsWR.combineTools as combineTools
-#import ExoAnalysis.cmsWR.backgroundFits as bgFits
 import numpy as np
 
 import sys
 
 
-
-lumi = 1900
 #MWRs = [ 800, 1000, 1200, 1400, 1600, 1800, 2000, 2400, 2600, 2800, 3000, 3200, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5600, 5800, 6000]
 MWRs = [ 2000]
+#MWRs = [ WRMASS]
+
 
 
 from itertools import combinations, product
@@ -37,7 +36,7 @@ for MWR in MWRs:
 
 	#np.linspace returns a numpy array of evenly spaced numbers which span the interval specified by the first two arguments
 	#the third arg specifies the number of elements in the array
-	low_mass_bins = np.linspace(.4,1, 31)*MWR
+	low_mass_bins = np.linspace(.4,1, 21)*MWR
 	hi_mass_bins = np.linspace(1,1.4, 21)*MWR
 	#mass_bins = range(int(MWR - search), int(MWR + (search + binsize)), int(binsize))
 	#for low_m, high_m in combinations(mass_bins, 2):
@@ -49,7 +48,6 @@ for MWR in MWRs:
 		if (high_m - low_m) > 2000: continue
 		low_bin = signal.FindBin(low_m)
 		hi_bin = signal.FindBin(high_m) - 1
-		#original signal_tuple = ("WR_eejj", signal.Integral(low_bin, hi_bin) * (0.01/50000.) * lumi)
 		
 		#events in signal and bkgnd histos are already weighted
 		signal_tuple = ("WR_eejj", signal.Integral(low_bin, hi_bin))
@@ -57,10 +55,6 @@ for MWR in MWRs:
 		bg_tuples.append(("dy", dy.Integral(low_bin, hi_bin) ))
 		bg_tuples.append(("top", top.Integral(low_bin, hi_bin) ))
 
-		#bg_tuples.append( ("TTbar", bgFits.TTbarFit.Integral(low_m, high_m)/bgFits.binWidth* (lumi /2000.) ))
-		#bg_tuples.append( ("DY",    bgFits.DYFit.Integral(low_m, high_m)   /bgFits.binWidth* (lumi /2000.) ))
-		#bg_tuples.append( ("AllBG",  bgFits.All_ee.Integral(low_m, high_m) * (lumi /2000.) ))
-		
 		#now sum the total number of bkgnd events
 		nBG = sum( [ b for a,b in bg_tuples ] )
 
@@ -72,11 +66,12 @@ for MWR in MWRs:
 		#last arg decides whether or not systematics are added to the datacard
 		sig, bgs = combineTools.makeDataCardSingleBin(datacard_file, "eejj", nBG, signal_tuple, bg_tuples, False)
 		method = "ProfileLikelihood"
-		#in command defined below, the sequence "-S", "0" means no systematic effects are evaluated
-		#in command defined below, the sequence "-n", datacard sets the job name equal to datacard
-		command = ["combine","-M",method,"-S","0",datacard_file,"-n", datacard,"-t","1000"]
-		#ret = combineTools.runCombine(command,identifier)
-		#(mean, meanError), (onesig_minus,onesig_plus), (twosig_minus,twosig_plus) = ret
-		#
-		#print MWR, MWR/2, low_m, high_m, sig, ("%s "*len(bgs)) % bgs , twosig_minus, onesig_minus, mean, onesig_plus, twosig_plus
-		#subprocess.check_output(["rm", "higgsCombine" + datacard + "." + method + ".mH120.123456.root"])
+		ntoys = "200"
+		#in command defined below: "-S0" disables evaluation of systematics, "-M %s" sets the combine run mode to the string 
+		#stored in method, "-n %s" sets the job name equal to the string datacard
+		command = "combine -M %s -S0 %s -n %s --toys %s" %(method, datacard_file, datacard, ntoys)
+		ret = combineTools.runCombine(command,datacard)
+		mean, (onesig_minus,onesig_plus), (twosig_minus,twosig_plus) = ret
+		
+		print MWR, MWR/2, low_m, high_m, sig, ("%s "*len(bgs)) % bgs , twosig_minus, onesig_minus, mean, onesig_plus, twosig_plus
+		subprocess.check_output(["rm", "higgsCombine" + datacard + "." + method + ".mH120.123456.root"])
