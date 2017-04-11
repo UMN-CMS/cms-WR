f246c34ebde18e84ca777121a9b642a1c53cf89d
skalafut <kalaf004@umn.edu>
2017-04-11 18:26:15 +0200

    added MuMuJJ channel to workflow

--- scripts/forGenSimToHLTtoRecoToMiniAodStep/makeAndSubmitGenSimToHLTToRecoToMiniAodJobs.sh
+++ scripts/forGenSimToHLTtoRecoToMiniAodStep/makeAndSubmitGenSimToHLTToRecoToMiniAodJobs.sh
@@ -15,6 +15,7 @@ do
 	while [ $jobId -le $nJobs ]
 	do
 
+		#############EEJJ#################3
 		#make python config file for GEN-SIM step                     
 		eval "sed 's/MASSWR/${WrMasses[$h]}/g'  WR_M-UNDEF_ToLNu_M-UNDEF_GEN-SIM_EEJJ.py > WR_tempEEJJ.py"   
 		eval "sed -i 's@NUM@$jobId@g' WR_tempEEJJ.py"
@@ -48,22 +49,50 @@ do
 		eval "sed 's@MASSNU@${NuMasses[$h]}@g' jobThreeEEJJ.sh > runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.sh"
 		rm jobOneEEJJ.sh jobTwoEEJJ.sh jobThreeEEJJ.sh
 
-		#move to test/ dir and make a skim cfg python file
-		#eval "cd ../../test/"
-		#eval "sed 's@MASSWR@${WrMasses[$h]}@g' temp_skims_cfg.py > skimsOne.py"
-		#eval "sed 's@MASSNU@${NuMasses[$h]}@g' skimsOne.py > skimsTwo.py"
-		#eval "sed 's@CHNL@EEJJ@g' skimsTwo.py > skimsThree.py"
-		#eval "sed 's@NNN@$jobId@g' skimsThree.py > skimsFour.py"
-		#eval "sed 's@TAGNAME@WRtoEEJJ_${WrMasses[$h]}_${NuMasses[$h]}_PrivReco@g' skimsFour.py > skims_cfg_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.py"
-		#rm skimsOne.py skimsTwo.py skimsThree.py skimsFour.py
-	
-		##move back to starting dir
-		#eval "cd -"
+		#submit job to queue
+		#echo "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAodToSkimToMinitree_EEJJ_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.sh"
+		eval "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAodToSkimToMinitree_EEJJ_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.sh"
+
+
+		#############MuMuJJ#################3
+		#make python config file for GEN-SIM step                     
+		eval "sed 's/MASSWR/${WrMasses[$h]}/g'  WR_M-UNDEF_ToLNu_M-UNDEF_GEN-SIM_MuMuJJ.py > WR_tempMuMuJJ.py"   
+		eval "sed -i 's@NUM@$jobId@g' WR_tempMuMuJJ.py"
+		eval "sed 's/MASSNU/${NuMasses[$h]}/g'  WR_tempMuMuJJ.py > WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_GEN-SIM_MuMuJJ_${jobId}.py"
+		rm WR_tempMuMuJJ.py
+
+		#make python config file for HLT step                     
+		eval "sed 's/MASSWR/${WrMasses[$h]}/g'  WR_M-UNDEF_ToLNu_M-UNDEF_HLT_MuMuJJ.py > WR_tempMuMuJJ.py"   
+		eval "sed -i 's@NUM@$jobId@g' WR_tempMuMuJJ.py"
+		eval "sed 's/MASSNU/${NuMasses[$h]}/g'  WR_tempMuMuJJ.py > WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_HLT_MuMuJJ_${jobId}.py"
+		rm WR_tempMuMuJJ.py
+
+		#make python config file for RECO step                     
+		eval "sed 's/MASSWR/${WrMasses[$h]}/g'  WR_M-UNDEF_ToLNu_M-UNDEF_RECO_MuMuJJ.py > WR_tempMuMuJJ.py"   
+		eval "sed -i 's@NUM@$jobId@g' WR_tempMuMuJJ.py"
+		eval "sed 's/MASSNU/${NuMasses[$h]}/g'  WR_tempMuMuJJ.py > WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_RECO_MuMuJJ_${jobId}.py"
+		rm WR_tempMuMuJJ.py
+
+		#make python config file for MINIAOD step                     
+		eval "sed 's/MASSWR/${WrMasses[$h]}/g'  WR_M-UNDEF_ToLNu_M-UNDEF_miniAOD_MuMuJJ.py > WR_tempMuMuJJ.py"   
+		eval "sed -i 's@NUM@$jobId@g' WR_tempMuMuJJ.py"
+		eval "sed 's/MASSNU/${NuMasses[$h]}/g'  WR_tempMuMuJJ.py > WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_miniAOD_MuMuJJ_${jobId}.py"
+		rm WR_tempMuMuJJ.py
+
+
+		#make bash script which tells batch job what to do
+		eval "sed 's@LOCALPATH@$jobStartingDir@' runBatchJobTempMuMuJJ.sh > jobOneMuMuJJ.sh"
+		eval "sed 's@PROXYPATH@$gridProxyPath@' jobOneMuMuJJ.sh > jobTwoMuMuJJ.sh"
+		eval "sed -i 's@NUM@$jobId@g' jobTwoMuMuJJ.sh"
+		eval "sed 's@MASSWR@${WrMasses[$h]}@g' jobTwoMuMuJJ.sh > jobThreeMuMuJJ.sh"
+		eval "sed 's@MASSNU@${NuMasses[$h]}@g' jobThreeMuMuJJ.sh > runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_MuMuJJ_${jobId}.sh"
+		rm jobOneMuMuJJ.sh jobTwoMuMuJJ.sh jobThreeMuMuJJ.sh
 
 		#submit job to queue
-		#echo "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAod_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.sh"
-		eval "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAod_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_EEJJ_${jobId}.sh"
-	
+		#echo "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAodToSkimToMinitree_MuMuJJ_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_MuMuJJ_${jobId}.sh"
+		eval "bsub -R 'rusage[mem=1500] && pool>2000' -q 1nh -J genSimToHltToRecoToMiniAodToSkimToMinitree_MuMuJJ_${jobId}_${WrMasses[$h]}_${NuMasses[$h]} < runBatchJob_WR_M-${WrMasses[$h]}_ToLNu_M-${NuMasses[$h]}_MuMuJJ_${jobId}.sh"
+
+
 		#increment jobId number
 		let jobId=jobId+1
 	done
---
+++ scripts/forGenSimToHLTtoRecoToMiniAodStep/runBatchJobTempMuMuJJ.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#use this file with a command like
+#bsub -R "rusage[mem=8000]" -q 8nh -J jobName < scriptName.sh
+UP=LOCALPATH
+#export X509_USER_PROXY=PROXYPATH
+
+cd $UP
+eval `scramv1 runtime -sh`
+eval "cmsenv"
+
+#after each new root file is made, move the file to /tmp/skalafut to avoid problems with core dumps and exceeding local disk space limit
+
+#make the GEN-SIM root file, then delete GEN-SIM python file
+eval "cmsRun WR_M-MASSWR_ToLNu_M-MASSNU_GEN-SIM_MuMuJJ_NUM.py"
+mv WR_M-MASSWR_ToLNu_M-MASSNU_GEN-SIM_13TeV-2016_MuMuJJ_NUM.root /tmp/skalafut/.
+rm WR_M-MASSWR_ToLNu_M-MASSNU_GEN-SIM_MuMuJJ_NUM.py
+
+#run HLT on GEN-SIM root file, then delete HLT py file and GENSIM root file made in previous step
+eval "cmsRun WR_M-MASSWR_ToLNu_M-MASSNU_HLT_MuMuJJ_NUM.py"
+mv WR_M-MASSWR_ToLNu_M-MASSNU_RAWSIM_13TeV-2016_MuMuJJ_NUM.root /tmp/skalafut/.
+rm WR_M-MASSWR_ToLNu_M-MASSNU_HLT_MuMuJJ_NUM.py /tmp/skalafut/WR_M-MASSWR_ToLNu_M-MASSNU_GEN-SIM_13TeV-2016_MuMuJJ_NUM.root
+
+#run RECO on RAW-SIM root file, then delete RECO py file and HLT root file made in previous step
+eval "cmsRun WR_M-MASSWR_ToLNu_M-MASSNU_RECO_MuMuJJ_NUM.py"
+mv WR_M-MASSWR_ToLNu_M-MASSNU_AODSIM_13TeV-2016_MuMuJJ_NUM.root /tmp/skalafut/.
+rm WR_M-MASSWR_ToLNu_M-MASSNU_RECO_MuMuJJ_NUM.py /tmp/skalafut/WR_M-MASSWR_ToLNu_M-MASSNU_RAWSIM_13TeV-2016_MuMuJJ_NUM.root
+
+#run miniAODSIM compression on AODSIM root file, then delete miniAOD py file and AODSIM root file made in previous step
+eval "cmsRun WR_M-MASSWR_ToLNu_M-MASSNU_miniAOD_MuMuJJ_NUM.py"
+mv WR_M-MASSWR_ToLNu_M-MASSNU_miniAOD_13TeV-2016_MuMuJJ_NUM.root /tmp/skalafut/.
+rm WR_M-MASSWR_ToLNu_M-MASSNU_miniAOD_MuMuJJ_NUM.py /tmp/skalafut/WR_M-MASSWR_ToLNu_M-MASSNU_AODSIM_13TeV-2016_MuMuJJ_NUM.root
+
+#run WR skim on miniAODSIM file produced by previous step
+eval "cd ../../"
+eval "cmsRun test/temp_skims_cfg.py files=file:/tmp/skalafut/WR_M-MASSWR_ToLNu_M-MASSNU_miniAOD_13TeV-2016_MuMuJJ_NUM.root output=WRtoMuMuJJ_MASSWR_MASSNU_PrivReco_skimPartNUM.root"
+mv WRtoMuMuJJ_MASSWR_MASSNU_PrivReco_skimPartNUM.root /tmp/skalafut/.
+rm /tmp/skalafut/WR_M-MASSWR_ToLNu_M-MASSNU_miniAOD_13TeV-2016_MuMuJJ_NUM.root
+
+#process WR skim file into WR minitrees
+eval "cmsRun test/temp_runAnalysis_cfg.py isMC=1 datasetTag=WRtoMuMuJJ_MASSWR_MASSNU_PrivReco files=file:/tmp/skalafut/WRtoMuMuJJ_MASSWR_MASSNU_PrivReco_skimPartNUM.root output=WRtoMuMuJJ_MASSWR_MASSNU_PrivReco_PartNUM.root"
+rm /tmp/skalafut/WRtoMuMuJJ_MASSWR_MASSNU_PrivReco_skimPartNUM.root
+
