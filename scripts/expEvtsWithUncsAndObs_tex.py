bdd28638d5147c1ab1699cf481d9496b2825a58f
skalafut <kalaf004@umn.edu>
2017-05-10 23:05:26 +0200

    updated exp evts with uncs and obs tex script to print out more decimal places for high mass WR

--- scripts/expEvtsWithUncsAndObs_tex.py
+++ scripts/expEvtsWithUncsAndObs_tex.py
@@ -20,8 +20,9 @@ obsEEFile = "selected_tree_data_signal_eeEE.root"
 
 ##now make a LaTex table with the following format (copied twice, once for each lepton channel):
 ## WR mass hypothesis | exp WR evts +/- unc | exp DY evts +/- unc | exp top evts +/- unc | exp bkgnd +/- unc | data |
-tex= "\\centering\n\\begin{{tabular}}{{{col}}}\n{table}\\end{{tabular}}"
-header = "  \\multirow{3}{2cm}{\\boldsymbol{\\MWR} \\bfseries(GeV)} & \\multirow{3}{*}{} & \\multicolumn{5}{c|}{Electron channel} & \\multicolumn{5}{c|}{Muon channel}  \\\\\n & \\multirow{2}{2cm}{Signal events (stat + syst)} & \\multirow{2}{2cm}{$Z/\\gamma^*$ events (stat + syst)} & \\multirow{2}{2cm}{Top quark events (stat + syst)} & \\multirow{2}{2cm}{Total bkgnd events (stat + syst)} & \\multirow{2}{2cm}{Data events} & \\multirow{2}{2cm}{Signal events (stat + syst)} & \\multirow{2}{2cm}{$Z/\\gamma^*$ events (stat + syst)} & \\multirow{2}{2cm}{Top quark events (stat + syst)} & \\multirow{2}{2cm}{Total bkgnd events (stat + syst)} & \\multirow{2}{2cm}{Data events} \\\\\n"
+tex= "\\begin{{tabular}}{{{col}}}\n{table}\\end{{tabular}}"
+header = " & \\multicolumn{5}{c|}{Electron channel}  \\\\\n \\MWR (GeV) & Signal (mean+/-stat+/-syst) & $Z/\\gamma^*$ (mean+/-stat+/-syst) & Top quark (mean+/-stat+/-syst) & All BG (mean+/-stat+/-syst) & Data \\\\\\hline\n"
+muonHeader = " & \\multicolumn{5}{c|}{Muon channel}  \\\\\n \\MWR (GeV) & Signal (mean+/-stat+/-syst) & $Z/\\gamma^*$ (mean+/-stat+/-syst) & Top quark (mean+/-stat+/-syst) & All BG (mean+/-stat+/-syst) & Data \\\\\\hline\n"
 table = {}  #dictionary named table
 masses = set()	#unordered sequence which cannot be indexed, and does not record order of insertion or element position
 index = 0   #mass window index used to read correct element in an array branch from root file
@@ -31,6 +32,8 @@ with open("configs/mass_cuts.txt") as f:
 		
 		#now split the iterator named line into four strings, two of which (low and hi) will not be used
 		ch, mass, low, hi = line.split()
+		intMass = int(mass)
+		if mass == "0": continue
 
 		#initialize floating point variables to hold the number of mean expected and observed evts, and uncertainties in a mass window
 		numWREvts = 0.0
@@ -51,24 +54,32 @@ with open("configs/mass_cuts.txt") as f:
 		#the mass_cuts.txt file is ordered from lowest mass to highest mass, so a simple index which increments after each
 		#iteration of this loop will allow the correct array element to be read from the input root files
 		if ch == 'EE':
+			wrEEFile = ("selected_tree_WRtoEEJJ_%i_%i_signal_eeEE.root" % (intMass, intMass/2) )
+			numWREvts = combineTools.getBranchMean(fileDir+wrEEFile, treeName, "NEventsInRange["+str(index)+"]")
+			wrStatUnc = combineTools.getBranchMean(fileDir+wrEEFile, treeName, "ErrorEventsInRange["+str(index)+"]")
+			wrSystUnc = math.sqrt(pow(0.037*numWREvts, 2) + pow(combineTools.getBranchStdDev(fileDir+wrEEFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
 			numDYEvts = combineTools.getBranchMean(fileDir+expDyEEFile, treeName, "NEventsInRange["+str(index)+"]")
 			dyStatUnc = combineTools.getBranchMean(fileDir+expDyEEFile, treeName, "ErrorEventsInRange["+str(index)+"]")
-			dySystUnc = math.sqrt(pow(0.41*combineTools.getBranchMean(fileDir+expDyEEFile, treeName, "NEventsInRange["+str(index)+"]"), 2) + pow(combineTools.getBranchStdDev(fileDir+expDyEEFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
+			dySystUnc = math.sqrt(pow(0.41*numDYEvts, 2) + pow(combineTools.getBranchStdDev(fileDir+expDyEEFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
 			numTopEvts = (0.4315)*(combineTools.getBranchMean(fileDir+expTopEEFile, treeName, "NEventsInRange["+str(index)+"]"))
 			topStatUnc = (0.4315)*(combineTools.getBranchMean(fileDir+expTopEEFile, treeName, "ErrorEventsInRange["+str(index)+"]"))
-			topSystUnc = math.sqrt( pow(0.1*(0.4315)*(combineTools.getBranchMean(fileDir+expTopEEFile, treeName, "NEventsInRange["+str(index)+"]")), 2)  + pow((0.4315)*(combineTools.getBranchStdDev(fileDir+expTopEEFile, treeName, "NEventsInRange["+str(index)+"]")), 2) )
+			topSystUnc = math.sqrt( pow(0.1*(0.4315)*numTopEvts, 2)  + pow((0.4315)*(combineTools.getBranchStdDev(fileDir+expTopEEFile, treeName, "NEventsInRange["+str(index)+"]")), 2) )
 			if numTopEvts < 0.1 : topStatUnc = 0.4315
 			numExpEvts = numDYEvts + numTopEvts
 			expEvtStatUnc = math.sqrt(pow(dyStatUnc,2) + pow(topStatUnc,2) )
 			expEvtSystUnc = math.sqrt(pow(dySystUnc,2) + pow(topSystUnc,2) )
 			numObsEvts = combineTools.getBranchMean(fileDir+obsEEFile, treeName, "NEventsInRange["+str(index)+"]")
 		if ch == 'MuMu':
+			wrMuMuFile = ("selected_tree_WRtoMuMuJJ_%i_%i_signal_mumuMuMu.root" % (intMass, intMass/2) )
+			numWREvts = combineTools.getBranchMean(fileDir+wrMuMuFile, treeName, "NEventsInRange["+str(index)+"]")
+			wrStatUnc = combineTools.getBranchMean(fileDir+wrMuMuFile, treeName, "ErrorEventsInRange["+str(index)+"]")
+			wrSystUnc = math.sqrt(pow(0.037*numWREvts, 2) + pow(combineTools.getBranchStdDev(fileDir+wrMuMuFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
 			numDYEvts = combineTools.getBranchMean(fileDir+expDyMuMuFile, treeName, "NEventsInRange["+str(index)+"]")
 			dyStatUnc = combineTools.getBranchMean(fileDir+expDyMuMuFile, treeName, "ErrorEventsInRange["+str(index)+"]")
-			dySystUnc = math.sqrt(pow(0.41*combineTools.getBranchMean(fileDir+expDyMuMuFile, treeName, "NEventsInRange["+str(index)+"]"), 2) + pow(combineTools.getBranchStdDev(fileDir+expDyMuMuFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
+			dySystUnc = math.sqrt(pow(0.41*numDYEvts, 2) + pow(combineTools.getBranchStdDev(fileDir+expDyMuMuFile, treeName, "NEventsInRange["+str(index)+"]"), 2) )
 			numTopEvts = (0.6592)*(combineTools.getBranchMean(fileDir+expTopMuMuFile, treeName, "NEventsInRange["+str(index)+"]"))
 			topStatUnc = (0.6592)*(combineTools.getBranchMean(fileDir+expTopMuMuFile, treeName, "ErrorEventsInRange["+str(index)+"]"))
-			topSystUnc = math.sqrt( pow(0.1*(0.6592)*(combineTools.getBranchMean(fileDir+expTopMuMuFile, treeName, "NEventsInRange["+str(index)+"]")), 2)  + pow((0.6592)*(combineTools.getBranchStdDev(fileDir+expTopMuMuFile, treeName, "NEventsInRange["+str(index)+"]")), 2) )
+			topSystUnc = math.sqrt( pow(0.1*(0.6592)*numTopEvts, 2)  + pow((0.6592)*(combineTools.getBranchStdDev(fileDir+expTopMuMuFile, treeName, "NEventsInRange["+str(index)+"]")), 2) )
 			if numTopEvts < 0.1 : topStatUnc = 0.6592
 			numExpEvts = numDYEvts + numTopEvts
 			expEvtStatUnc = math.sqrt(pow(dyStatUnc,2) + pow(topStatUnc,2) )
@@ -85,15 +96,26 @@ with open("configs/mass_cuts.txt") as f:
 		#now add a new key and value to the dictionary. the key is a pair containing the mass and lepton channel, and the value is a string containing the number of expected and observed events
 		#the numbers 6 and 7 control how many digits and white space are shown in each dict value
 		#round controls the number of decimal points shown
-		table[(mass , ch)] = "& {sig:<6} & {dy:<6} & {top:<6} & {total:<6} & {obs:<7}".format(sig = (str(round(numWREvts,2))+" +/- "+str(round(wrStatUnc,2))+" +/- "+str(round(wrSystUnc,2)) ), dy = (str(round(numDYEvts,2))+" +/- "+str(round(dyStatUnc,2))+" +/- "+str(round(dySystUnc,2))), top = (str(round(numTopEvts,2))+" +/- "+str(round(topStatUnc,2))+" +/- "+str(round(topSystUnc,2))), total = (str(round(numExpEvts,2))+" +/- "+str(round(expEvtStatUnc,2))+" +/- "+str(round(expEvtSystUnc,2))), obs=str(numObsEvts))
+		if mass != "4400" and mass != "4600" and mass != "4800" and mass != "5000" and mass != "5200" and mass != "5600" and mass != "5800" and mass != "6000": table[(mass , ch)] = "& {sig:<6} & {dy:<6} & {top:<6} & {total:<6} & {obs:<7}".format(sig = (str(round(numWREvts,2))+" +/- "+str(round(wrStatUnc,2))+" +/- "+str(round(wrSystUnc,2)) ), dy = (str(round(numDYEvts,2))+" +/- "+str(round(dyStatUnc,2))+" +/- "+str(round(dySystUnc,2))), top = (str(round(numTopEvts,2))+" +/- "+str(round(topStatUnc,2))+" +/- "+str(round(topSystUnc,2))), total = (str(round(numExpEvts,2))+" +/- "+str(round(expEvtStatUnc,2))+" +/- "+str(round(expEvtSystUnc,2))), obs=str(numObsEvts))
+		if mass == "4400" or mass == "4600" or mass == "4800" or mass == "5000" or mass == "5200" or mass == "5600" or mass == "5800" or mass == "6000": table[(mass , ch)] = "& {sig:<6} & {dy:<6} & {top:<6} & {total:<6} & {obs:<7}".format(sig = (str(round(numWREvts,4))+" +/- "+str(round(wrStatUnc,4))+" +/- "+str(round(wrSystUnc,4)) ), dy = (str(round(numDYEvts,2))+" +/- "+str(round(dyStatUnc,2))+" +/- "+str(round(dySystUnc,2))), top = (str(round(numTopEvts,2))+" +/- "+str(round(topStatUnc,2))+" +/- "+str(round(topSystUnc,2))), total = (str(round(numExpEvts,2))+" +/- "+str(round(expEvtStatUnc,2))+" +/- "+str(round(expEvtSystUnc,2))), obs=str(numObsEvts))
+		
 		masses.add(mass)
 #end reading mass_cuts.txt
 
 #define the table structure (number of columns and column titles)
+#do electron channel first
 tex_table = header
 for mass in sorted(masses):
 	if mass == "0": continue
-	tex_table  += "{mass:4d} {ee} {mumu} \\\\ \\hline\n".format(mass=int(mass), ee=table[(mass, "EE")], mumu = table[(mass, "MuMu")])
+	#tex_table  += "{mass:4d} {ee} {mumu} \\\\ \\hline\n".format(mass=int(mass), ee=table[(mass, "EE")], mumu = table[(mass, "MuMu")])
+	tex_table += "{mass:4d} {ee} \\\\ \\hline\n".format(mass=int(mass), ee=table[(mass, "EE")])
+	
+	
+#now do muon channel
+tex_table += muonHeader
+for mass in sorted(masses):
+	if mass == "0": continue
+	tex_table += "{mass:4d} {mumu} \\\\ \\hline\n".format(mass=int(mass), mumu=table[(mass, "MuMu")])
+	
 
-
-print tex.format(table=tex_table,   col="c|c|c|c|c|c|c|c|c|c|c|")
+print tex.format(table=tex_table,   col="|c|c|c|c|c|c|")
